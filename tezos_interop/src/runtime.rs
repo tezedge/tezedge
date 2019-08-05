9d0019b45cfd57a6b356ebce262fa84d305bf8ed
Tomas Sedlak <tomas.sedlak@simplestaking.com>
2019-08-05 11:50:41 +0200

    Add documentation

--- tezos_interop/src/runtime.rs
+++ tezos_interop/src/runtime.rs
@@ -10,10 +10,14 @@ use ocaml::core::callback::caml_startup;
 use lazy_static::lazy_static;
 
 lazy_static! {
+    /// Because ocaml runtime should be accessed only by a single thread
+    /// we will create the `OCAML_ENV` singleton.
     static ref OCAML_ENV: OcamlEnvironment = initialize_environment();
 }
 
-pub fn initialize_ocaml() {
+/// This function will start ocaml runtime.
+/// Ocaml runtime should always be called from a single thread.
+pub fn startup_ocaml() {
     let mut ptr = ptr::null_mut();
     let argv: *mut *mut u8 = &mut ptr;
     unsafe {
@@ -21,21 +25,19 @@ pub fn initialize_ocaml() {
     }
 }
 
-pub struct OcamlResult<T> where T: Send {
+/// The result received from ocaml side.
+/// Value is not available immediately but caller will have to await for it.
+pub struct OcamlResult<T>
+    where T: Send
+{
     result: Arc<Mutex<Option<T>>>,
-    state: Arc<Mutex<SharedState>>
-}
-
-pub struct OcamlTask {
-    future: Box<dyn FnOnce() + Send + 'static>,
     state: Arc<Mutex<SharedState>>,
 }
 
-struct SharedState {
-    waker: Option<Waker>
-}
-
-impl <T> Future for OcamlResult<T> where T: Send {
+/// Allows the caller to use `await` on ocaml result.
+impl<T> Future for OcamlResult<T>
+    where T: Send
+{
     type Output = T;
 
     fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
@@ -51,13 +53,54 @@ impl <T> Future for OcamlResult<T> where T: Send {
     }
 }
 
+/// Ocaml task is executed by `OcamlThreadExecutor`. Task holds future responsible
+/// for executing ocaml function(s) and passing the result back to rust.
+struct OcamlTask {
+    future: Box<dyn FnOnce() + Send + 'static>,
+    state: Arc<Mutex<SharedState>>,
+}
 
+impl OcamlTask {
+    /// Create new ocaml task
+    ///
+    /// # Arguments
+    ///
+    /// * `f` - the function will be executed in ocaml thread context
+    /// * `f_result_holder` - will hold result of the `f` after `f`'s completion
+    /// * `shared_state` - shared state between `OcamlTask` and `OcamlResult`
+    fn new<F, T>(f: F, f_result_holder: Arc<Mutex<Option<T>>>, shared_state: Arc<Mutex<SharedState>>) -> OcamlTask
+        where
+            F: FnOnce() -> T + Send + 'static,
+            T: Send + 'static
+    {
+        let ocaml_future = Box::new(move || {
+            let mut result = f_result_holder.lock().unwrap();
+            *result = Some(f());
+        });
+        OcamlTask {
+            future: ocaml_future,
+            state: shared_state,
+        }
+    }
+}
+
+/// This struct represents a shared state between `OcamlTask` and `OcamlResult`.
+struct SharedState {
+    waker: Option<Waker>
+}
+
+/// Runs `OcamlTask` to it's completion. By design of this library there will be
+/// only a single instance of `OcamlThreadExecutor` running because ocaml runtime
+/// is not designed to be accessed from multiple threads.
 struct OcamlThreadExecutor {
+
+    /// Receiver is used to receive tasks which will be then executed
+    /// in the ocaml runtime.
     ready_tasks: Receiver<OcamlTask>
 }
 
 impl OcamlThreadExecutor {
-
+    /// Runs scheduled ocaml task to it's completion.
     fn run(&self) {
         while let Ok(task) = self.ready_tasks.recv() {
             task.future.call_once(());
@@ -66,23 +109,31 @@ impl OcamlThreadExecutor {
             }
         }
     }
-
 }
 
+/// Spawns ocaml task. Spawning is simply sending ocaml task into the sender queue `spawned_tasks`.
+/// Ocaml tasks are then received and executed by the `OcamlThreadExecutor` singleton.
 struct OcamlTaskSpawner {
+
+    /// Sender is used to send tasks to the `OcamlThreadExecutor`.
     spawned_tasks: Arc<Mutex<Sender<OcamlTask>>>
 }
 
 impl OcamlTaskSpawner {
+
+    /// Spawns ocaml task. Spawning is simply sending ocaml task into the sender queue `spawned_tasks`.
+    /// Ocaml tasks are then received and executed by the `OcamlThreadExecutor` singleton.
     pub fn spawn(&self, task: OcamlTask) -> Result<(), SendError<OcamlTask>> {
         self.spawned_tasks.lock().unwrap().send(task)
     }
 }
 
+/// Holds data related to ocaml environment.
 struct OcamlEnvironment {
     spawner: OcamlTaskSpawner
 }
 
+/// Create the environment and initialize ocaml runtime.
 fn initialize_environment() -> OcamlEnvironment {
     let (task_tx, task_rx) = channel();
     let spawner = OcamlTaskSpawner { spawned_tasks: Arc::new(Mutex::new(task_tx)) };
@@ -95,24 +146,16 @@ fn initialize_environment() -> OcamlEnvironment {
     OcamlEnvironment { spawner }
 }
 
-pub fn run<T, F>(f: F) -> OcamlResult<T>
+/// Run a function in ocaml runtime and return a result.
+pub fn run<F, T>(f: F) -> OcamlResult<T>
     where
         F: FnOnce() -> T + Send + 'static,
         T: Send + 'static
 {
-
-
     let result = Arc::new(Mutex::new(None));
     let state = Arc::new(Mutex::new(SharedState { waker: None }));
     let result_future = OcamlResult { result: result.clone(), state: state.clone() };
-    let ocaml_future = Box::new(move || {
-        let mut result = result.lock().unwrap();
-        *result = Some(f());
-    });
-    let task = OcamlTask {
-        future: ocaml_future,
-        state: state.clone()
-    };
+    let task = OcamlTask::new(f, result, state);
     OCAML_ENV.spawner.spawn(task).expect("Failed to spawn task");
 
     result_future
