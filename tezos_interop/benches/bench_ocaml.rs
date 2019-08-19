8546f6a72872b94214d0c2cb6b951eb0b026c26c
Tomas Sedlak <tomas.sedlak@simplestaking.com>
2019-08-19 15:42:20 +0200

    Benchmark tezos_interop by calling "echo" OCaml function

---
+++ tezos_interop/benches/bench_ocaml.rs
@@ -0,0 +1,27 @@
+#![feature(async_await, test)]
+
+extern crate test;
+
+use test::Bencher;
+
+use ocaml::Str;
+
+use tezos_interop::runtime;
+use tezos_interop::runtime::OcamlResult;
+
+fn ocaml_fn_echo(arg: String) -> OcamlResult<String> {
+    runtime::spawn(move || {
+        let ocaml_function = ocaml::named_value("echo").expect("function 'echo' is not registered");
+        let ocaml_result: Str = ocaml_function.call::<Str>(arg.as_str().into()).unwrap().into();
+        ocaml_result.as_str().to_string()
+    })
+}
+
+#[bench]
+fn bench_ocaml_echo(b: &mut Bencher) {
+    // Run one dummy task so ocaml runtime is started. We do not want to measure
+    // runtime startup time but only a time of a method call.
+    futures::executor::block_on(ocaml_fn_echo("__dummy__".into()));
+
+    b.iter(|| futures::executor::block_on(ocaml_fn_echo("Hello world!".into())));
+}
