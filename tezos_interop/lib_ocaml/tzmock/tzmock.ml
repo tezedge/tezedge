1fbdee3b4d3f94f29f08995fd53dc6a02a079909
Branislav Kontur <branislav.kontur@simplestaking.com>
2019-09-08 00:51:29 +0200

    WIP - storage configuration and initialization done + realn get_block_header impl

--- tezos_interop/lib_ocaml/tzmock/tzmock.ml
+++ tezos_interop/lib_ocaml/tzmock/tzmock.ml
@@ -1,15 +1,13 @@
 exception Exn of string
 
-let twice = fun x -> 2 * x
+let health_check = fun x: string -> "UP - " ^ x
 
-let echo = fun x: string -> x
+let get_block_header _block_header_hash =
+    "0000000201dd9fb5edc4f29e7d28f41fe56d57ad172b7686ed140ad50294488b68de29474d000000005c017cd804683625c2445a4e9564bf710c5528fd99a7d150d2a2a323bc22ff9e2710da4f6d0000001100000001000000000800000000000000029bd8c75dec93c276d2d8e8febc3aa6c9471cb2cb42236b3ab4ca5f1f2a0892f6000500000003ba671eef00d6a8bea20a4677fae51268ab6be7bd8cfc373cd6ac9e0a00064efcc404e1fb39409c5df255f7651e3d1bb5d91cb2172b687e5d56ebde58cfd92e1855aaafbf05"
 
-let encode_operation x =
-    if x = "02f2b231cb7e4a6a264ca85e3e69ef4bec6cfed8749a5ce605f43b2ca4b4d93308000002298c03ed7d454a101eb7022bc95f7e5f41ac78f80901d84f0080bd83140000e7670f32038107a59a2b9cfefae36ea21f5aa63c00931399163d0698672e8532aa15ecc679dccd6844634099e02d224cc61f6bce236819bc855e09c222f2d4056ea608c0de9d5cd633652b29cf3e1c80c6c1e0340c"
-    then "{ \"contents\":\n    [ { \"kind\": \"transaction\",\n        \"source\": \"tz1KqTpEZ7Yob7QbPE4Hy4Wo8fHG8LhKxZSx\", \"fee\": \"1272\",\n        \"counter\": \"1\", \"gas_limit\": \"10200\", \"storage_limit\": \"0\",\n        \"amount\": \"42000000\",\n        \"destination\": \"tz1gjaF81ZRRvdzjobyfVNsAeSC6PScjfQwN\" } ],\n  \"signature\":\n    \"sighEKUX4TVUc7hHHTb9GX1VJSYbDteH5TAXTA7bnhJo81X7mC99hnroqR4QoBkivq7xRr8ZeGC5oXsoBQao6ha9RHAk3i4c\" }"
-    else raise (Exn "ohnoes!")
+let apply_block (_block_header : string) (_operations : string array array) =
+    "lvl 2, fit 2, prio 5, 0 ops"
 
-
-let _ = Callback.register "twice" twice
-let _ = Callback.register "echo" echo
-let _ = Callback.register "encode_operation" encode_operation
+let _ = Callback.register "health_check" health_check
+let _ = Callback.register "get_block_header" get_block_header
+let _ = Callback.register "apply_block" apply_block
