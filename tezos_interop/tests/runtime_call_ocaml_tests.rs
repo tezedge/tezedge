d87ae9cb944def8ae087c9c4aa52b21cff2233a4
Branislav Kontur <branislav.kontur@simplestaking.com>
2019-09-13 01:27:51 +0200

    TN-35 - tests for error handling

--- tezos_interop/tests/runtime_call_ocaml_tests.rs
+++ tezos_interop/tests/runtime_call_ocaml_tests.rs
@@ -1,4 +1,3 @@
-use ocaml::Str;
 use tezos_interop::runtime;
 
 #[test]
@@ -11,13 +10,22 @@ fn can_complete_future_with_return_value() {
 }
 
 #[test]
-fn can_call_ocaml_fn_echo() {
-    let arg = "Hello ocaml!";
-    let ocaml_future = runtime::spawn(move || {
-        let ocaml_function = ocaml::named_value("echo").expect("function 'echo' not registered");
-        let ocaml_result: Str = ocaml_function.call::<Str>(arg.into()).unwrap().into();
-        ocaml_result.as_str().to_string()
-    });
-    let ocaml_result = futures::executor::block_on(ocaml_future);
-    assert_eq!("Hello ocaml!", &ocaml_result)
+#[should_panic]
+fn can_complete_future_with_unregistered_function() {
+    futures::executor::block_on(
+        runtime::spawn(|| {
+            let _ = ocaml::named_value("this_function_is_surelly_not_registered_in_ocaml_runtime")
+                .expect("function 'this_function_is_surelly_not_registered_in_ocaml_runtime' is not registered");
+        })
+    );
+}
+
+#[test]
+#[should_panic]
+fn can_complete_future_with_error() {
+    futures::executor::block_on(
+        runtime::spawn(|| {
+            panic!("Error occurred");
+        })
+    );
 }
