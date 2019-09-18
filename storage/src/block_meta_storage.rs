2a92374caa4fcd5988b40a3aea0e4a1975d13e77
Tomas Sedlak <tomas.sedlak@simplestaking.com>
2019-09-18 21:39:57 +0200

    Change implementation to allow both the predecessor and the successor to be optional

--- storage/src/block_meta_storage.rs
+++ storage/src/block_meta_storage.rs
@@ -32,43 +32,48 @@ impl BlockMetaStorage {
 
 }
 
-const MASK_IS_PROCESSED: u8  = 0b0000_0001;
-const MASK_HAS_SUCCESSOR: u8 = 0b0000_0010;
+const BLOCK_LEN: usize = 32;
+
+const MASK_IS_PROCESSED: u8    = 0b0000_0001;
+const MASK_HAS_SUCCESSOR: u8   = 0b0000_0010;
+const MASK_HAS_PREDECESSOR: u8 = 0b0000_0100;
 
 const IDX_MASK: usize = 0;
 const IDX_PREDECESSOR: usize = IDX_MASK + 1;
-const IDX_SUCCESSOR: usize = IDX_PREDECESSOR + 32;
+const IDX_SUCCESSOR: usize = IDX_PREDECESSOR + BLOCK_LEN;
 
-const LEN_NO_SUCCESSOR: usize = 1 + 32 + 0;
-const LEN_WITH_SUCCESSOR: usize = 1 + 32 + 32;
+const BLANK_BLOCK_HASH: [u8; BLOCK_LEN] = [0; BLOCK_LEN];
+const META_LEN: usize = 1 + BLOCK_LEN + BLOCK_LEN;
+
+macro_rules! is_processed {
+    ($mask:expr) => {{ ($mask & MASK_IS_PROCESSED) != 0 }}
+}
+macro_rules! has_predecessor {
+    ($mask:expr) => {{ ($mask & MASK_HAS_PREDECESSOR) != 0 }}
+}
+macro_rules! has_successor {
+    ($mask:expr) => {{ ($mask & MASK_HAS_SUCCESSOR) != 0 }}
+}
 
 /// Meta information for the block
 #[derive(Clone, PartialEq, Debug)]
 pub struct Meta {
-    predecessor: BlockHash,
+    predecessor: Option<BlockHash>,
     successor: Option<BlockHash>,
     is_processed: bool,
 }
 
 /// Codec for `Meta`
 ///
-/// * bytes layout: `[mask(1)][predecessor(32)][opt_successor(32)]`
+/// * bytes layout: `[mask(1)][predecessor(32)][successor(32)]`
 impl Codec for Meta {
     fn decode(bytes: &[u8]) -> Result<Self, SchemaError> {
-        if LEN_NO_SUCCESSOR <= bytes.len() {
+        if META_LEN == bytes.len() {
             let mask = bytes[IDX_MASK];
-            let is_processed = (mask & MASK_IS_PROCESSED) != 0;
-            let predecessor = bytes[IDX_PREDECESSOR..IDX_SUCCESSOR].to_vec();
+            let is_processed = is_processed!(mask);
+            let predecessor = if has_predecessor!(mask) { Some(bytes[IDX_PREDECESSOR..IDX_SUCCESSOR].to_vec()) } else { None };
+            let successor = if has_successor!(mask) { Some(bytes[IDX_SUCCESSOR..META_LEN].to_vec()) } else { None };
 
-            let successor = if (mask & MASK_HAS_SUCCESSOR) != 0 {
-                if LEN_WITH_SUCCESSOR == bytes.len() {
-                    Some(bytes[IDX_SUCCESSOR..LEN_WITH_SUCCESSOR].to_vec())
-                } else {
-                    return Err(SchemaError::DecodeError);
-                }
-            } else {
-                None
-            };
             Ok(Meta { predecessor, successor, is_processed })
         } else {
             Err(SchemaError::DecodeError)
@@ -76,14 +81,27 @@ impl Codec for Meta {
     }
 
     fn encode(&self) -> Result<Vec<u8>, SchemaError> {
-        let mut value = vec![];
+        let mut mask = 0u8;
+        if self.is_processed {
+            mask |= MASK_IS_PROCESSED;
+        }
+        if self.predecessor.is_some() {
+            mask |= MASK_HAS_PREDECESSOR;
+        }
+        if self.successor.is_some() {
+            mask |= MASK_HAS_SUCCESSOR;
+        }
 
-        let byte_is_processed = if self.is_processed { MASK_IS_PROCESSED } else { 0 };
-        let byte_has_successor = if self.successor.is_some() { MASK_HAS_SUCCESSOR } else { 0 };
-        let mask = byte_is_processed | byte_has_successor;
+        let mut value = Vec::with_capacity(META_LEN);
         value.push(mask);
-        value.extend(&self.predecessor);
-        self.successor.as_ref().map(|successor| value.extend(successor));
+        match &self.predecessor {
+            Some(predecessor) =>  value.extend(predecessor),
+            None => value.extend(&BLANK_BLOCK_HASH)
+        }
+        match &self.successor {
+            Some(successor) =>  value.extend(successor),
+            None => value.extend(&BLANK_BLOCK_HASH)
+        }
 
         Ok(value)
     }
@@ -107,20 +125,22 @@ fn merge_meta_value(_new_key: &[u8], existing_val: Option<&[u8]>, operands: &mut
     for op in operands {
         match result {
             Some(ref mut val) => {
-                assert!(val.len() == LEN_NO_SUCCESSOR || val.len() == LEN_WITH_SUCCESSOR, "Value length is incorrect");
-                assert!(val.len() <= op.len(), "Only extension of the value length is allowed");
+                assert_eq!(META_LEN, val.len(), "Value length is incorrect");
+
+                let mask_val = val[IDX_MASK];
+                let mask_op = op[IDX_MASK];
 
                 // merge `mask(1)`
-                let mask = val[IDX_MASK] | op[IDX_MASK];
+                val[IDX_MASK] = mask_val | mask_op;
 
-                if val.len() == op.len() {
-                    val.clear();
-                    val.extend(op);
-                } else if op.len() == LEN_WITH_SUCCESSOR {
-                    val.extend(&op[IDX_SUCCESSOR..LEN_WITH_SUCCESSOR]);
+                // if op has predecessor and val has not, copy it from op to val
+                if has_predecessor!(mask_op) && !has_predecessor!(mask_val) {
+                    val.splice(IDX_PREDECESSOR..IDX_SUCCESSOR, op[IDX_PREDECESSOR..IDX_SUCCESSOR].iter().cloned());
                 }
-
-                val[IDX_MASK] = mask;
+                // if op has successor and val has not, copy it from op to val
+                if has_successor!(mask_op) && !has_successor!(mask_val) {
+                    val.splice(IDX_SUCCESSOR..META_LEN, op[IDX_SUCCESSOR..META_LEN].iter().cloned());
+                }
             },
             None => result = Some(op.to_vec())
         }
@@ -141,7 +161,7 @@ mod tests {
     fn block_meta_encoded_equals_decoded() -> Result<(), Error> {
         let expected = Meta {
             is_processed: false,
-            predecessor: vec![98; 32],
+            predecessor: Some(vec![98; 32]),
             successor: Some(vec![21; 32])
         };
         let encoded_bytes = expected.encode()?;
@@ -167,7 +187,7 @@ mod tests {
             let k = vec![44; 32];
             let mut v = Meta {
                 is_processed: false,
-                predecessor: vec![98; 32],
+                predecessor: None,
                 successor: None
             };
             let p = BlockMetaStorageDatabase::merge(&db, &k, &v);
@@ -176,13 +196,17 @@ mod tests {
             v.successor = Some(vec![21; 32]);
             let _ = BlockMetaStorageDatabase::merge(&db, &k, &v);
             v.is_processed = false;
+            v.predecessor = Some(vec![98; 32]);
+            v.successor = None;
+            let _ = BlockMetaStorageDatabase::merge(&db, &k, &v);
+            v.predecessor = None;
             let m = BlockMetaStorageDatabase::merge(&db, &k, &v);
             assert!(m.is_ok());
             match BlockMetaStorageDatabase::get(&db, &k) {
                 Ok(Some(value)) => {
                     let expected = Meta {
                         is_processed: true,
-                        predecessor: vec![98; 32],
+                        predecessor: Some(vec![98; 32]),
                         successor: Some(vec![21; 32])
                     };
                     assert_eq!(expected, value);
