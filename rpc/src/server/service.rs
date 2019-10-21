0d9ffcf0b966b8fc4c68e46f12b84c56b0f9b007
Martin Lacko <martin.lacko@simplestaking.com>
2019-10-21 10:14:32 +0200

    Changed parameter handling in the server

--- rpc/src/server/service.rs
+++ rpc/src/server/service.rs
@@ -1,8 +1,6 @@
 use hyper::{Body, Response, Error, Server, Request, StatusCode, Method};
 use hyper::service::{service_fn, make_service_fn};
 use std::net::SocketAddr;
-use lazy_static::lazy_static;
-use std::sync::Mutex;
 use futures::Future;
 use crate::rpc_actor::RpcServerRef;
 use riker::actors::ActorSystem;
@@ -12,38 +10,25 @@ use chrono::prelude::*;
 use crate::encoding::base_types::*;
 use tezos_encoding::hash::{HashEncoding, HashType};
 
-lazy_static! {
-    static ref ACTOR_SYSTEM: Mutex<Option<ActorSystem>> = Mutex::new(None);
-    static ref RPC_ACTOR: Mutex<Option<RpcServerRef>> = Mutex::new(None);
-}
-
 type ServiceResult = Result<Response<Body>, Box<dyn std::error::Error + Sync + Send>>;
 
 pub fn spawn_server(addr: &SocketAddr, sys: ActorSystem, actor: RpcServerRef) -> impl Future<Output=Result<(), Error>> {
-    let mut state = ACTOR_SYSTEM.lock().unwrap();
-    *state = Some(sys);
-
-    let mut state = RPC_ACTOR.lock().unwrap();
-    *state = Some(actor);
-
     Server::bind(addr)
-        .serve(make_service_fn(|_| async move {
-            Ok::<_, Error>(service_fn(move |req| {
-                async move {
-                    router(req).await
-                }
-            }))
+        .serve(make_service_fn(move |_| {
+            let sys = sys.clone();
+            let actor = actor.clone();
+            async move {
+                let sys = sys.clone();
+                let actor = actor.clone();
+                Ok::<_, Error>(service_fn(move |req| {
+                    let sys = sys.clone();
+                    let actor = actor.clone();
+                    async move {
+                        router(req, sys, actor).await
+                    }
+                }))
+            }
         }))
-}
-
-fn get_sys() -> ActorSystem {
-    (*ACTOR_SYSTEM.lock().expect("Unable to communicate with actor system")).clone()
-        .expect("Actor system not set properly")
-}
-
-fn get_actor() -> RpcServerRef {
-    (*RPC_ACTOR.lock().expect("Unable to contact rpc actor")).clone()
-        .expect("RPC actor not set properly")
 }
 
 fn timestamp() -> TimeStamp {
@@ -62,17 +47,15 @@ fn empty() -> ServiceResult {
         .body(Body::empty())?)
 }
 
-async fn active_chains() -> ServiceResult {
+async fn active_chains(_sys: ActorSystem, _actor: RpcServerRef) -> ServiceResult {
     Ok(Response::new(Body::from("10101010")))
 }
 
-async fn bootstrapped() -> ServiceResult {
+async fn bootstrapped(sys: ActorSystem, actor: RpcServerRef) -> ServiceResult {
     use crate::server::control_msg::GetCurrentHead;
     use crate::encoding::monitor::BootstrapInfo;
 
-    let sys = get_sys();
-    let rpc_actor = get_actor();
-    let current_head = ask(&sys, &rpc_actor, GetCurrentHead::Request).await;
+    let current_head = ask(&sys, &actor, GetCurrentHead::Request).await;
     if let GetCurrentHead::Response(current_head) = current_head {
         let resp = serde_json::to_string(&if let Some(current_head) = current_head {
             let hash = HashEncoding::new(HashType::BlockHash).bytes_to_string(&current_head.hash());
@@ -87,10 +70,10 @@ async fn bootstrapped() -> ServiceResult {
 }
 
 
-async fn router(req: Request<Body>) -> ServiceResult {
+async fn router(req: Request<Body>, sys: ActorSystem, actor: RpcServerRef) -> ServiceResult {
     match (req.method(), req.uri().path()) {
-        (&Method::GET, "/monitor/active_chains") => active_chains().await,
-        (&Method::GET, "/monitor/bootstrapped") => bootstrapped().await,
+        (&Method::GET, "/monitor/active_chains") => active_chains(sys, actor).await,
+        (&Method::GET, "/monitor/bootstrapped") => bootstrapped(sys, actor).await,
         _ => not_found()
     }
 }
