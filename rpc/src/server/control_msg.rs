9df51c0edc1d7a666e1440768838736b580af5d9
Tomas Sedlak <tomas.sedlak@simplestaking.com>
2019-11-27 22:18:02 +0100

    Untangle data retrieval in RPC from the actor system

--- rpc/src/server/control_msg.rs
+++ rpc/src/server/control_msg.rs
@@ -4,7 +4,6 @@
 use shell::shell_channel::BlockApplied;
 
 use crate::helpers::FullBlockInfo;
-use tezos_context::channel::ContextAction;
 
 /// Request/Response to access the Current Head data from RpcActor
 #[derive(Debug, Clone)]
@@ -20,25 +19,4 @@ pub enum GetFullCurrentHead {
     Response(Option<FullBlockInfo>),
 }
 
-/// Request list of block header hashes. Will retrieve only applied blocks.
-#[derive(Debug, Clone)]
-pub enum GetBlocks {
-    Request {
-        /// Optional starting block hash (formatted string), if left out then we will
-        /// assume genesis block hash.
-        block_hash: Option<String>,
-        /// Required limit of blocks to retrieve.
-        limit: usize
-    },
-    Response(Vec<FullBlockInfo>)
-}
 
-/// Request actions for a specific block
-#[derive(Debug, Clone)]
-pub enum GetBlockActions {
-    Request {
-        /// Block hash formatted as a string
-        block_hash: String,
-    },
-    Response(Vec<ContextAction>),
-}
--- rpc/src/server/mod.rs
+++ rpc/src/server/mod.rs
@@ -1,5 +1,9 @@
+// Copyright (c) SimpleStaking and Tezedge Contributors
+// SPDX-License-Identifier: MIT
+
 mod service;
 mod ask;
 pub mod control_msg;
 
 pub use service::spawn_server;
+pub use service::RpcServiceEnvironment;
