ab649e103cede72f8f4dd0628101497168a26b74
Juraj Selep <jurajselep@gmail.com>
2019-12-03 01:28:53 +0100

    Fixed docker compose

--- docker/tezedge/tezedge.sh
+++ docker/tezedge/tezedge.sh
@@ -1,15 +1,24 @@
-#!/bin/sh
+#!/bin/bash
 
-# delete tezos-data 
-rm -rf /var/tezedge/tezos-data/*
+# light node config
+NETWORK="babylonnet"
+TEZOS_DIR="/tmp/tezedge/tezos-data"
+BOOTSTRAP_DIR="/tmp/tezedge/tezedge-data/"
+IDENTITY_FILE="/tmp/tezedge/identity.json"
+CONFIG_FILE="./light_node/etc/tezedge/tezedge.config"
 
-# delete tezedge-data 
-rm -rf /var/tezedge/tezedge-data/*
+# cleanup data directory
+rm -rf $BOOTSTRAP_DIR && mkdir $BOOTSTRAP_DIR 
+rm -rf $TEZOS_DIR && mkdir $TEZOS_DIR
 
-# run tezedge node 
-light-node \
-    --network="babylonnet" \
-    --protocol-runner="/bin/protocol-runner" \
-    --tezos-data-dir="/var/tezedge/tezos-data/" \
-    --bootstrap-db-path="/var/tezedge/tezedge-data/" \
-    --identity="/var/tezedge/identity.json" 
+# protocol_runner needs 'libtezos.so' to run
+export LD_LIBRARY_PATH="/home/appuser/tezedge/tezos/interop/lib_tezos/artifacts:/home/appuser/tezedge/target/release"
+# start node
+cargo run --release --bin light-node -- \
+                            --config-file "$CONFIG_FILE" \
+                            --tezos-data-dir "$TEZOS_DIR" \
+                            --identity-file "$IDENTITY_FILE" \
+                            --bootstrap-db-path "$BOOTSTRAP_DIR" \
+                            --network "$NETWORK" \
+                            --ocaml-log-enabled "true" \
+                            --protocol-runner "./target/release/protocol-runner"
