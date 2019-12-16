3cd53da617314b93262d355a48f0d089bfe64d2e
Branislav Kontur <branislav.kontur@simplestaking.com>
2019-12-16 20:56:43 +0100

    Added encodings for contracts and big_maps

--- tezos/client/tests/decode_context_data_test.rs
+++ tezos/client/tests/decode_context_data_test.rs
@@ -110,6 +110,14 @@ fn test_fn_decode_context_data() {
         ).unwrap().unwrap()
     );
 
+    assert_eq!(
+        "0".to_string(),
+        client::decode_context_data(
+            protocol("PsBABY5HQTSkA4297zNHfsZNKtxULfL18y95qb3m53QJiXGmrbU"),
+            key("data, block_priority"),
+            data("0000"),
+        ).unwrap().unwrap()
+    );
 
     assert_eq!(
         "\"inited\"".to_string(),
@@ -202,6 +210,15 @@ fn test_fn_decode_context_data_rolls_and_cycles() {
             protocol("PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP"),
             key("data, cycle, 0, last_roll, 0"),
             data("00000a8c"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "[\"nceVLU2CcmkFGAC4raDPbNMmutpnG3rzbfqPnK7aiCZAipFccp2TK\",\"tz1gT2uVzSqBq3ZdH6uG4uJq8bqLga9XKrrq\",\"0\",\"0\"]".to_string(),
+        client::decode_context_data(
+            protocol("PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS"),
+            key("data, cycle, 4, nonces, 8768"),
+            data("00927e7e2e7df224676fc98f8f823fef5a49ceaeaae50249303c2a40dac1bee8c100e4463e7bea3a75cebb112159a7ec6c29225a12410000"),
         ).unwrap().unwrap()
     );
 }
@@ -349,6 +366,118 @@ fn test_fn_decode_context_data_contracts() {
             protocol("PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP"),
             key("data, contracts, index, ed25519, 89, b5, 12, 22, 97, e589f9ba8b91f4bf74804da2fe8d4a, frozen_balance, 0, rewards"),
             data("00"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "\"0\"".to_string(),
+        client::decode_context_data(
+            protocol("PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP"),
+            key("data, contracts, index, ed25519, 89, b5, 12, 22, 97, e589f9ba8b91f4bf74804da2fe8d4a, paid_bytes"),
+            data("00"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "\"232\"".to_string(),
+        client::decode_context_data(
+            protocol("PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP"),
+            key("data, contracts, index, ed25519, 89, b5, 12, 22, 97, e589f9ba8b91f4bf74804da2fe8d4a, used_bytes"),
+            data("a803"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "202".to_string(),
+        client::decode_context_data(
+            protocol("PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP"),
+            key("data, contracts, index, ed25519, 89, b5, 12, 22, 97, e589f9ba8b91f4bf74804da2fe8d4a, len, code"),
+            data("000000ca"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "[{\"prim\":\"parameter\",\"args\":[{\"prim\":\"or\",\"args\":[{\"prim\":\"lambda\",\"args\":[{\"prim\":\"unit\"},{\"prim\":\"list\",\"args\":[{\"prim\":\"operation\"}]}],\"annots\":[\"%do\"]},{\"prim\":\"unit\",\"annots\":[\"%default\"]}]}]},{\"prim\":\"storage\",\"args\":[{\"prim\":\"key_hash\"}]},{\"prim\":\"code\",\"args\":[[[[{\"prim\":\"DUP\"},{\"prim\":\"CAR\"},{\"prim\":\"DIP\",\"args\":[[{\"prim\":\"CDR\"}]]}]],{\"prim\":\"IF_LEFT\",\"args\":[[{\"prim\":\"PUSH\",\"args\":[{\"prim\":\"mutez\"},{\"int\":\"0\"}]},{\"prim\":\"AMOUNT\"},[[{\"prim\":\"COMPARE\"},{\"prim\":\"EQ\"}],{\"prim\":\"IF\",\"args\":[[],[[{\"prim\":\"UNIT\"},{\"prim\":\"FAILWITH\"}]]]}],[{\"prim\":\"DIP\",\"args\":[[{\"prim\":\"DUP\"}]]},{\"prim\":\"SWAP\"}],{\"prim\":\"IMPLICIT_ACCOUNT\"},{\"prim\":\"ADDRESS\"},{\"prim\":\"SENDER\"},[[{\"prim\":\"COMPARE\"},{\"prim\":\"EQ\"}],{\"prim\":\"IF\",\"args\":[[],[[{\"prim\":\"UNIT\"},{\"prim\":\"FAILWITH\"}]]]}],{\"prim\":\"UNIT\"},{\"prim\":\"EXEC\"},{\"prim\":\"PAIR\"}],[{\"prim\":\"DROP\"},{\"prim\":\"NIL\",\"args\":[{\"prim\":\"operation\"}]},{\"prim\":\"PAIR\"}]]}]]}]".to_string(),
+        client::decode_context_data(
+            protocol("PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP"),
+            key("data, contracts, index, ed25519, 89, b5, 12, 22, 97, e589f9ba8b91f4bf74804da2fe8d4a, data, code"),
+            data("000000c602000000c105000764085e036c055f036d0000000325646f046c000000082564656661756c740501035d050202000000950200000012020000000d03210316051f02000000020317072e020000006a0743036a00000313020000001e020000000403190325072c020000000002000000090200000004034f0327020000000b051f02000000020321034c031e03540348020000001e020000000403190325072c020000000002000000090200000004034f0327034f0326034202000000080320053d036d0342"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "30".to_string(),
+        client::decode_context_data(
+            protocol("PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP"),
+            key("data, contracts, index, ed25519, 89, b5, 12, 22, 97, e589f9ba8b91f4bf74804da2fe8d4a, len, storage"),
+            data("0000001e"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "{\"bytes\":\"00b2e19a9e74440d86c59f13dab8a18ff873e889ea\"}".to_string(),
+        client::decode_context_data(
+            protocol("PsddFKi32cMJ2qPjf43Qv5GDWLDPZb3T3bF6fLKiF5HtvHNU7aP"),
+            key("data, contracts, index, ed25519, 89, b5, 12, 22, 97, e589f9ba8b91f4bf74804da2fe8d4a, data, storage"),
+            data("0000001a0a0000001500b2e19a9e74440d86c59f13dab8a18ff873e889ea"),
+        ).unwrap().unwrap()
+    );
+}
+
+#[test]
+#[serial]
+fn test_fn_decode_context_data_big_maps() {
+    assert_eq!(
+        "\"14\"".to_string(),
+        client::decode_context_data(
+            protocol("PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS"),
+            key("data, big_maps, next"),
+            data("0e"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "\"228\"".to_string(),
+        client::decode_context_data(
+            protocol("PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS"),
+            key("data, big_maps, index, f5, c8, 90, 54, 17, 93, 9, total_bytes"),
+            data("a403"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "{\"prim\":\"address\"}".to_string(),
+        client::decode_context_data(
+            protocol("PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS"),
+            key("data, big_maps, index, f5, c8, 90, 54, 17, 93, 9, key_type"),
+            data("036e"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "{\"prim\":\"pair\",\"args\":[{\"prim\":\"nat\"},{\"prim\":\"map\",\"args\":[{\"prim\":\"address\"},{\"prim\":\"nat\"}]}]}".to_string(),
+        client::decode_context_data(
+            protocol("PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS"),
+            key("data, big_maps, index, f5, c8, 90, 54, 17, 93, 9, value_type"),
+            data("076503620760036e0362"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "77".to_string(),
+        client::decode_context_data(
+            protocol("PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS"),
+            key("data, big_maps, index, 64, 22, 06, 31, 4f, 53, 4, contents, 8f, 8e, 6a, 2e, 70, dd1df0b8ce0025ff9fd2ad956525abd0165a7d536534a60cf099d1, len"),
+            data("0000004d"),
+        ).unwrap().unwrap()
+    );
+
+    assert_eq!(
+        "{\"prim\":\"Pair\",\"args\":[{\"prim\":\"Pair\",\"args\":[{\"bytes\":\"00001f67eb5af692ec0cab1c5743fcab82bfd20b2d61\"},{\"bytes\":\"0000b855ca20b48b2d22ff633f1e8f1be30b747ca722\"}]},{\"prim\":\"Pair\",\"args\":[{\"prim\":\"Pair\",\"args\":[{\"int\":\"7020000\"},{\"int\":\"1570129462\"}]},{\"int\":\"20000\"}]}]}".to_string(),
+        client::decode_context_data(
+            protocol("PsBabyM1eUXZseaJdmXFApDSBqj8YBfwELoxZHHW77EMcAbbwAS"),
+            key("data, big_maps, index, 64, 22, 06, 31, 4f, 53, 4, contents, 8f, 8e, 6a, 2e, 70, dd1df0b8ce0025ff9fd2ad956525abd0165a7d536534a60cf099d1, data"),
+            data("070707070a0000001600001f67eb5af692ec0cab1c5743fcab82bfd20b2d610a000000160000b855ca20b48b2d22ff633f1e8f1be30b747ca7220707070700a0f7d80600b698b2d90b00a0b802"),
         ).unwrap().unwrap()
     );
 }
