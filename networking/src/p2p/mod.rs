66b6443862e84313a080ad2cd0044fd3ad69ca0e
Tomas Sedlak <tomas.sedlak@simplestaking.com>
2020-02-06 09:24:12 +0100

    Write documentation for the peer module

--- networking/src/p2p/mod.rs
+++ networking/src/p2p/mod.rs
@@ -1,6 +1,8 @@
 // Copyright (c) SimpleStaking and Tezedge Contributors
 // SPDX-License-Identifier: MIT
 
+//! This module handles low level p2p communication.
+
 mod stream;
 pub mod peer;
 pub mod network_channel;
