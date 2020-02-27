46defab996d22da67668c927c21b1fdf2a841ca2
Juraj Selep <jurajselep@gmail.com>
2020-02-27 20:57:08 +0100

    Added identity to docker

--- docker/compose/tezedge.sh
+++ docker/compose/tezedge.sh
@@ -9,6 +9,8 @@ NETWORK="babylonnet"
 TEZOS_DIR="/tmp/tezedge/tezos-data"
 BOOTSTRAP_DIR="/tmp/tezedge/tezedge-data/"
 CONFIG_FILE="./light_node/etc/tezedge/tezedge.config"
+IDENTITY_FILE="/tmp/tezedge/identity.json"
+
 
 # cleanup data directory
 rm -rf $BOOTSTRAP_DIR && mkdir $BOOTSTRAP_DIR 
@@ -22,6 +24,7 @@ cargo run --release --bin light-node -- \
                             --config-file "$CONFIG_FILE" \
                             --tezos-data-dir "$TEZOS_DIR" \
                             --bootstrap-db-path "$BOOTSTRAP_DIR" \
+                            --identity-file "$IDENTITY_FILE" \
                             --network "$NETWORK" \
                             --ocaml-log-enabled "false" \
                             --protocol-runner "./target/release/protocol-runner"
