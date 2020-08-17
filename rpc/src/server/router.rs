b85a0b7cbfbf3e53b33172bb63d2f1ee0e866806
Adrian Nagy <adrian.nagy@simplestaking.com>
2020-08-17 10:52:22 +0200

    Add PR (sandbox baking) feature to CHANGELOG

--- rpc/src/server/router.rs
+++ rpc/src/server/router.rs
@@ -11,9 +11,9 @@ use crate::server::{Handler, HResult, Params, Query, RpcServiceEnvironment};
 use crate::server::{dev_handler, handler};
 
 pub(crate) fn create_routes() -> PathTree<Handler> {
-
     let mut routes = PathTree::<Handler>::new();
-    // Tezos shell and protocol rpc
+
+    // Shell rpc - implemented
     routes.handle("/version", handler::node_version);
     routes.handle("/monitor/bootstrapped", handler::bootstrapped);
     routes.handle("/monitor/commit_hash", handler::commit_hash);
@@ -26,32 +26,34 @@ pub(crate) fn create_routes() -> PathTree<Handler> {
     routes.handle("/chains/:chain_id/blocks/:block_id/live_blocks", handler::live_blocks);
     routes.handle("/chains/:chain_id/blocks/:block_id/header", handler::chains_block_id_header);
     routes.handle("/chains/:chain_id/blocks/:block_id/header/shell", handler::chains_block_id_header_shell);
-    routes.handle("/chains/:chain_id/blocks/:block_id/context/constants", handler::context_constants);
-    routes.handle("/chains/:chain_id/blocks/:block_id/context/raw/bytes/cycle", handler::context_cycle);
-    routes.handle("/chains/:chain_id/blocks/:block_id/context/raw/bytes/rolls/owner/current", handler::rolls_owner_current);
-    routes.handle("/chains/:chain_id/blocks/:block_id/context/raw/json/cycle/:cycle_id", handler::cycle);
-    routes.handle("/chains/:chain_id/blocks/:block_id/helpers/baking_rights", handler::baking_rights);
-    routes.handle("/chains/:chain_id/blocks/:block_id/helpers/endorsing_rights", handler::endorsing_rights);
-    routes.handle("/chains/:chain_id/blocks/:block_id/votes/listings", handler::votes_listings);
     routes.handle("/chains/:chain_id/mempool/pending_operations", handler::mempool_pending_operations);
     routes.handle("/chains/:chain_id/blocks/:block_id/protocols", handler::get_block_protocols);
     routes.handle("/chains/:chain_id/blocks/:block_id/hash", handler::get_block_hash);
     routes.handle("/chains/:chain_id/blocks/:block_id/operation_hashes", handler::get_block_operation_hashes);
-    routes.handle("/chains/:chain_id/blocks/:block_id/context/contracts/:pkh/counter", handler::get_contract_counter);
-    routes.handle("/chains/:chain_id/blocks/:block_id/context/contracts/:pkh/manager_key", handler::get_contract_manager_key);
-
-    // injection rpcs
     routes.handle("/injection/operation", handler::inject_operation);
     routes.handle("/injection/block", handler::inject_block);
 
-    // routed rpcs through protocol (ffi calls) 
-    routes.handle("/chains/:chain_id/blocks/:block_id/helpers/current_level", handler::current_level);
-    routes.handle("/chains/:chain_id/blocks/:block_id/minimal_valid_time", handler::minimal_valid_time);
-    routes.handle("/chains/:chain_id/blocks/:block_id/helpers/scripts/run_operation", handler::run_operation);
+    // Shell rpcs - routed through ffi calls
     routes.handle("/chains/:chain_id/blocks/:block_id/helpers/preapply/operations", handler::preapply_operations);
     routes.handle("/chains/:chain_id/blocks/:block_id/helpers/preapply/block", handler::preapply_block);
 
-    // Tezedge dev and support rpc
+    // Protocol rpcs - implemented
+    routes.handle("/chains/:chain_id/blocks/:block_id/context/constants", handler::context_constants);
+    routes.handle("/chains/:chain_id/blocks/:block_id/context/raw/bytes/cycle", handler::context_cycle);
+    routes.handle("/chains/:chain_id/blocks/:block_id/context/raw/bytes/rolls/owner/current", handler::rolls_owner_current);
+    routes.handle("/chains/:chain_id/blocks/:block_id/context/raw/json/cycle/:cycle_id", handler::cycle);
+    routes.handle("/chains/:chain_id/blocks/:block_id/context/contracts/:pkh/counter", handler::get_contract_counter);
+    routes.handle("/chains/:chain_id/blocks/:block_id/context/contracts/:pkh/manager_key", handler::get_contract_manager_key);
+    routes.handle("/chains/:chain_id/blocks/:block_id/helpers/baking_rights", handler::baking_rights);
+    routes.handle("/chains/:chain_id/blocks/:block_id/helpers/endorsing_rights", handler::endorsing_rights);
+    routes.handle("/chains/:chain_id/blocks/:block_id/votes/listings", handler::votes_listings);
+
+    // Protocol rpcs - routed through ffi calls
+    routes.handle("/chains/:chain_id/blocks/:block_id/helpers/scripts/run_operation", handler::run_operation);
+    routes.handle("/chains/:chain_id/blocks/:block_id/helpers/current_level", handler::current_level);
+    routes.handle("/chains/:chain_id/blocks/:block_id/minimal_valid_time", handler::minimal_valid_time);
+
+    // Tezedge dev and support rpcs
     routes.handle("/dev/chains/main/blocks", dev_handler::dev_blocks);
     routes.handle("/dev/chains/main/actions/blocks/:block_hash", dev_handler::dev_action_cursor);
     routes.handle("/dev/chains/main/actions/contracts/:contract_address", dev_handler::dev_action_cursor);
@@ -69,7 +71,7 @@ trait Routes<Fut> {
 impl<T, F> Routes<T> for PathTree<Handler>
     where
         T: Fn(Request<Body>, Params, Query, RpcServiceEnvironment) -> F + Send + Sync + 'static,
-        F: Future<Output = HResult> + Send + 'static
+        F: Future<Output=HResult> + Send + 'static
 {
     fn handle(&mut self, path: &str, f: T) {
         self.insert(path, Arc::new(move |req, params, query, env| {
