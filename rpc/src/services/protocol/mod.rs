55dd854dcb3a643a3cbdf21a8e1d924a9097de3b
Michal Stanek <michal.stanek@simplestaking.com>
2020-09-03 01:09:21 +0200

    Fix compilation errors in RPC

--- rpc/src/services/protocol/mod.rs
+++ rpc/src/services/protocol/mod.rs
@@ -84,8 +84,7 @@ pub(crate) fn check_and_get_baking_rights(
         state,
     )?;
 
-    let context = TezedgeContext::new(BlockStorage::new(&persistent_storage), context_list.clone(),
-        persistent_storage.merkle());
+    let context = TezedgeContext::new(BlockStorage::new(&persistent_storage), persistent_storage.merkle());
 
     // split impl by protocol
     let hash: &str = &HashType::ProtocolHash.bytes_to_string(&context_proto_params.protocol_hash);
@@ -208,8 +207,7 @@ pub(crate) fn check_and_get_endorsing_rights(
         state,
     )?;
 
-    let context = TezedgeContext::new(BlockStorage::new(&persistent_storage), context_list.clone(),
-        persistent_storage.merkle());
+    let context = TezedgeContext::new(BlockStorage::new(&persistent_storage), persistent_storage.merkle());
 
     // split impl by protocol
     let hash: &str = &HashType::ProtocolHash.bytes_to_string(&context_proto_params.protocol_hash);
@@ -365,8 +363,7 @@ pub(crate) fn proto_get_contract_counter(
         state,
     )?;
 
-    let context = TezedgeContext::new(BlockStorage::new(&persistent_storage), context_list.clone(),
-        persistent_storage.merkle());
+    let context = TezedgeContext::new(BlockStorage::new(&persistent_storage), persistent_storage.merkle());
 
     // split impl by protocol
     let hash: &str = &HashType::ProtocolHash.bytes_to_string(&context_proto_params.protocol_hash);
@@ -409,8 +406,7 @@ pub(crate) fn proto_get_contract_manager_key(
         state,
     )?;
 
-    let context = TezedgeContext::new(BlockStorage::new(&persistent_storage), context_list.clone(),
-        persistent_storage.merkle());
+    let context = TezedgeContext::new(BlockStorage::new(&persistent_storage), persistent_storage.merkle());
 
     // split impl by protocol
     let hash: &str = &HashType::ProtocolHash.bytes_to_string(&context_proto_params.protocol_hash);
