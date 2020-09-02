482902d9db90745b613621b370b97f980b7c2ff4
Michal Stanek <michal.stanek@simplestaking.com>
2020-09-03 01:09:21 +0200

    Fix compilation errors in RPC

--- rpc/src/services/protocol/proto_005_2/contract_service.rs
+++ rpc/src/services/protocol/proto_005_2/contract_service.rs
@@ -2,11 +2,12 @@
 // SPDX-License-Identifier: MIT
 
 use std::string::ToString;
+use std::convert::TryInto;
 
 use failure::bail;
 
 use storage::skip_list::Bucket;
-use storage::context::{TezedgeContext, ContextIndex, ContextApi};
+use storage::context::{TezedgeContext, ContextApi};
 use tezos_messages::base::signature_public_key::SignaturePublicKey;
 use tezos_messages::p2p::binary_message::BinaryMessage;
 
@@ -18,13 +19,14 @@ pub(crate) fn get_contract_counter(context_proto_params: ContextProtocolParam, p
     // level of the block
     let level = context_proto_params.level;
 
-    let context_index = ContextIndex::new(Some(level), None);
+    // get context_hash from level
+    let ctx_hash = context.level_to_hash(level.try_into()?)?;
     
     let indexed_contract_key = construct_indexed_contract_key(pkh)?;
 
     // ["data","contracts","index","91","6e","d7","72","4e","49","0000535110affdb82923710d1ec205f26ba8820a2259","counter"]
     let contract_counter_key = vec![indexed_contract_key.clone(), "counter".to_string()];
-    let contract_counter = if let Some(Bucket::Exists(data)) = context.get_key(&context_index, &contract_counter_key)? {
+    let contract_counter = if let Some(data) = context.get_key_from_history(&ctx_hash, &contract_counter_key)? {
         Some(tezos_messages::protocol::proto_005_2::contract::Counter::from_bytes(data)?)
     } else {
         None
@@ -42,13 +44,14 @@ pub(crate) fn get_contract_manager_key(context_proto_params: ContextProtocolPara
     // level of the block
     let level = context_proto_params.level;
 
-    let context_index = ContextIndex::new(Some(level), None);
+    // get context_hash from level
+    let ctx_hash = context.level_to_hash(level.try_into()?)?;
     
     let indexed_contract_key = construct_indexed_contract_key(pkh)?;
 
     // ["data","contracts","index","91","6e","d7","72","4e","49","0000535110affdb82923710d1ec205f26ba8820a2259","manager"]
     let manager_key_key = vec![indexed_contract_key.clone(), "manager".to_string()];
-    if let Some(Bucket::Exists(data)) = context.get_key(&context_index, &manager_key_key)? {
+    if let Some(data) = context.get_key_from_history(&ctx_hash, &manager_key_key)? {
         match SignaturePublicKey::from_tagged_bytes(data) {
             Ok(pk) => {
                 Ok(Some(pk.to_string()))
