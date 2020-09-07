75ac3f655bf99788ffb160ac1b6afb32bd53e1c5
Bruno Deferrari <bruno.deferrari@simplestaking.com>
2020-09-07 11:02:49 -0300

    Small reorg

--- tezos/api/src/ocaml_conv/mod.rs
+++ tezos/api/src/ocaml_conv/mod.rs
@@ -1,11 +1,17 @@
 // Copyright (c) SimpleStaking and Tezedge Contributors
 // SPDX-License-Identifier: MIT
 
-use tezos_messages::p2p::encoding::operations_for_blocks::Path;
+use tezos_messages::p2p::encoding::{
+    block_header::BlockHeader, operation::Operation, operations_for_blocks::Path,
+};
 
 // FFI Wrappers
 #[repr(transparent)]
 pub struct FfiPath(pub Path);
+#[repr(transparent)]
+pub struct FfiBlockHeader(pub BlockHeader);
+#[repr(transparent)]
+pub struct FfiOperation(pub Operation);
 
 // Hashes
 struct OCamlHash {}
