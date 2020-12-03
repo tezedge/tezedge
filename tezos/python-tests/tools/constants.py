c9c7a8e0b7fd2cb1db6e137ed389d4bea86fe9f0
Adrian Nagy <adrian.nagy@simplestaking.com>
2020-12-03 11:17:11 +0100

    CI: update node params

--- tezos/python-tests/tools/constants.py
+++ tezos/python-tests/tools/constants.py
@@ -209,4 +209,6 @@ NODE_PARAMS = ['--sandbox-patch-context-json-file', paths.TEZOS_HOME + 'sandbox-
                '--ffi-pool-connection-timeout-in-secs=60', 
                '--ffi-pool-max-lifetime-in-secs=21600', 
                '--ffi-pool-idle-timeout-in-secs=1800', '--store-context-actions=false',
-               '--tokio-threads=0', '--enable-testchain=false', '--log-level=debug']
+               '--tokio-threads=0', '--enable-testchain=false', '--log-level=debug',
+               '--synchronization-thresh', '0']
+
