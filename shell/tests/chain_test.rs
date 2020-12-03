45f14504439de16c7b7d19d208ef0ea4a32c2c38
Adrian Nagy <adrian.nagy@simplestaking.com>
2020-12-03 12:10:05 +0100

    DBG: test configuration threshold to 0

--- shell/tests/chain_test.rs
+++ shell/tests/chain_test.rs
@@ -38,7 +38,7 @@ lazy_static! {
             disable_mempool: false,
             private_node: false,
             initial_peers: vec![],
-            peer_threshold: PeerConnectionThreshold::new(0, 10, None),
+            peer_threshold: PeerConnectionThreshold::new(0, 10, Some(0)),
         },
         NETWORK_VERSION.clone(),
     );
