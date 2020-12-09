e89a99423325f8fc718bf0b2690784121d67de84
Adrian Nagy <adrian.nagy@simplestaking.com>
2020-12-09 01:21:49 +0100

    remove unnecessarily long sleep in test - confused the bakers in tests

--- tezos/python-tests/tests/test_baker_endorser.py
+++ tezos/python-tests/tests/test_baker_endorser.py
@@ -41,9 +41,8 @@ class TestAllDaemonsWithOperations:
         for i in range(NUM_NODES):
             sandbox.add_node(i, params=constants.NODE_PARAMS)
         utils.activate_alpha(sandbox.client(0), parameters)
-        # TODO: TEZEDGE
-        # for now, tezedge node needs some time to apply and distributer the 1st level
-        time.sleep(30)
+        #
+        time.sleep(2)
         sandbox.add_baker(0, 'bootstrap5', proto=constants.ALPHA_DAEMON)
         sandbox.add_baker(1, 'bootstrap4', proto=constants.ALPHA_DAEMON)
         sandbox.add_endorser(0, account='bootstrap1', endorsement_delay=1,
