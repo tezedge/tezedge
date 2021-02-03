d3ba156a9297eaf914788709a3173582783cef90
Adrián Nagy <47247773+adonagy@users.noreply.github.com>
2021-02-03 16:28:09 +0100

    Fix - format light-node/src/main.rs (#416)

--- light_node/src/main.rs
+++ light_node/src/main.rs
@@ -328,9 +328,13 @@ fn block_on_actors(
         )
         .expect("Failed to create mempool prevalidator");
     }
-    let websocket_handler =
-        WebsocketHandler::actor(&actor_system, tokio_runtime.handle().clone(), env.rpc.websocket_address, log.clone())
-            .expect("Failed to start websocket actor");
+    let websocket_handler = WebsocketHandler::actor(
+        &actor_system,
+        tokio_runtime.handle().clone(),
+        env.rpc.websocket_address,
+        log.clone(),
+    )
+    .expect("Failed to start websocket actor");
     let _ = Monitor::actor(
         &actor_system,
         network_channel.clone(),
