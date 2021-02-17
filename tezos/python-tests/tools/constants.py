981b483985e894b696ede12c1505a45c70790164
Mateusz Nowakowski <mateusz.nowakowski@simplestaking.com>
2021-02-17 09:37:40 +0100

    update outdated parameter name

--- tezos/python-tests/tools/constants.py
+++ tezos/python-tests/tools/constants.py
@@ -211,7 +211,8 @@ NODE_PARAMS = ['--sandbox-patch-context-json-file', paths.TEZOS_HOME + 'sandbox-
                '--ffi-pool-max-connections=10',
                '--ffi-pool-connection-timeout-in-secs=60',
                '--ffi-pool-max-lifetime-in-secs=21600',
-               '--ffi-pool-idle-timeout-in-secs=1800', '--store-context-actions=false',
+               '--ffi-pool-idle-timeout-in-secs=1800',
+			   '--actions-store-backend=rocksdb',
                '--compute-context-action-tree-hashes=false',
                '--tokio-threads=0', '--enable-testchain=false', '--log-level=debug',
                '--synchronization-thresh', '0']
