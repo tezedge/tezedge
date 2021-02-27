56a4a77a20368c029d4a983677cbaf6d5487373f
mambisi <lilbizi96@gmail.com>
2021-02-27 07:35:30 +0000

    cargo fmt

--- storage/src/persistent/commit_log/writer.rs
+++ storage/src/persistent/commit_log/writer.rs
@@ -1,14 +1,14 @@
-use std::io::{BufWriter, Seek, SeekFrom, Write, BufReader, Read};
-use std::fs::{File, OpenOptions};
 use crate::persistent::commit_log::error::TezedgeCommitLogError;
-use std::path::{Path, PathBuf};
-use crate::persistent::commit_log::{Index, INDEX_FILE_NAME, DATA_FILE_NAME, TH_LENGTH};
+use crate::persistent::commit_log::{Index, DATA_FILE_NAME, INDEX_FILE_NAME, TH_LENGTH};
+use std::fs::{File, OpenOptions};
+use std::io::{BufWriter, Seek, SeekFrom, Write};
 use std::ops::Sub;
+use std::path::{Path, PathBuf};
 
 pub(crate) struct Writer {
     index_file: BufWriter<File>,
     data_file: BufWriter<File>,
-    last_index: i64
+    last_index: i64,
 }
 
 impl Writer {
@@ -17,7 +17,7 @@ impl Writer {
             std::fs::create_dir_all(dir.as_ref())?;
         }
         if dir.as_ref().exists() & !dir.as_ref().is_dir() {
-           return Err(TezedgeCommitLogError::PathError);
+            return Err(TezedgeCommitLogError::PathError);
         }
 
         let mut index_file_path = PathBuf::new();
@@ -42,46 +42,40 @@ impl Writer {
 
         let last_index = Self::last_index(index_file.try_clone()?);
 
-
         Ok(Self {
-            index_file : BufWriter::new(index_file),
+            index_file: BufWriter::new(index_file),
             data_file: BufWriter::new(data_file),
-            last_index
+            last_index,
         })
     }
 
     pub(crate) fn write(&mut self, buf: &[u8]) -> Result<u64, TezedgeCommitLogError> {
         if buf.len() > u64::MAX as usize {
-            return Err(TezedgeCommitLogError::MessageLengthError)
+            return Err(TezedgeCommitLogError::MessageLengthError);
         }
         let mut out = vec![];
+        let uncompressed_length = buf.len();
         {
             let mut wtr = snap::write::FrameEncoder::new(&mut out);
-            wtr.write_all(buf);
+            wtr.write_all(buf)?;
         }
         let message_len = out.len() as u64;
         let message_pos = self.data_file.seek(SeekFrom::End(0))?;
         self.data_file.write_all(&out)?;
-        let th = Index::new(message_pos, message_len);
+        let th = Index::new(message_pos, message_len, uncompressed_length as u64);
         self.index_file.seek(SeekFrom::End(0))?;
         self.index_file.write(&th.to_vec())?;
         self.last_index += 1;
         Ok(self.last_index as u64)
     }
 
-    fn last_index(index_file : File) -> i64{
-        let mut index_file_reader = BufReader::new(index_file);
-        index_file_reader.seek(SeekFrom::Start(0));
-        let mut indexes = vec![];
-        let mut buf = Vec::new();
-
-        index_file_reader.read_to_end(&mut buf);
-        let header_chunks = buf.chunks_exact(TH_LENGTH);
-        for chunk in header_chunks {
-            let th = Index::from_buf(chunk).unwrap();
-            indexes.push(th)
-        }
-        (indexes.len() as i64 ).sub(1)
+    pub fn last_index(index_file: File) -> i64 {
+        let metadata = match index_file.metadata() {
+            Ok(m) => m,
+            Err(_) => return -1,
+        };
+        let items_count = metadata.len() / (TH_LENGTH as u64);
+        (items_count as i64).sub(1)
     }
 
     pub(crate) fn flush(&mut self) -> Result<(), TezedgeCommitLogError> {
