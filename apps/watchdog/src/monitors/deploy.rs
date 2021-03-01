affdb9c1231c1ff0d466388f9854364a431b7022
Adrian Nagy <adrian.nagy@simplestaking.com>
2021-03-01 18:41:46 +0100

    Store all logs on crash on the filesystem

--- apps/watchdog/src/monitors/deploy.rs
+++ apps/watchdog/src/monitors/deploy.rs
@@ -2,11 +2,16 @@
 // SPDX-License-Identifier: MIT
 #![forbid(unsafe_code)]
 
+use std::fs::File;
+use std::io::Write;
+use std::path::Path;
 use std::path::PathBuf;
 
+use chrono::Utc;
 use failure::bail;
 use shiplift::Docker;
 use slog::{info, warn, Logger};
+use zip::write::ZipWriter;
 
 use crate::deploy_with_compose::{
     restart_sandbox, restart_stack, shutdown_and_update, shutdown_and_update_sandbox,
@@ -40,11 +45,59 @@ impl DeployMonitor {
         }
     }
 
-    async fn collect_node_logs(&self) -> Result<serde_json::Value, failure::Error> {
-        match reqwest::get("http://localhost:17732/v2/log?limit=100").await {
-            Ok(result) => Ok(result.json::<serde_json::Value>().await?),
-            Err(e) => bail!("Error collecting node logs from debugger: {:?}", e),
+    /// Collect node logs from the debugger with a step and stores them in a file
+    /// format: [[<latest to latest - LIMIT>], ..., [<LIMIT to 0>]]
+    async fn collect_node_logs(&self) -> Result<String, failure::Error> {
+        const LIMIT: usize = 2000;
+        if !Path::new("/tmp/tezedge-watchdog-logs").exists() {
+            std::fs::create_dir("/tmp/tezedge-watchdog-logs")?;
         }
+        let timestamp = Utc::now().time().to_string();
+        let file_name = format!("{}_crash.log", &timestamp);
+        let zip_name = format!("{}_crash.zip", &timestamp);
+        let file_path = format!("/tmp/tezedge-watchdog-logs/{}", &zip_name);
+
+        let mut zip_file = ZipWriter::new(File::create(&file_path)?);
+        let zip_options =
+            zip::write::FileOptions::default().compression_method(zip::CompressionMethod::Stored);
+
+        zip_file.start_file(&file_name, zip_options)?;
+
+        // enclose the arrays in an another array to make the file a valid json
+        zip_file.write("[".as_bytes())?;
+
+        // last log, get its cursor_id
+        let mut cursor_id: Option<usize> =
+            match reqwest::get("http://localhost:17732/v2/log?limit=1").await {
+                Ok(result) => {
+                    let res_json = result.json::<serde_json::Value>().await?;
+                    res_json[0]["id"].as_u64().map(|val| val as usize)
+                }
+                Err(e) => {
+                    bail!("Error collecting last: {:?}", e);
+                }
+            };
+
+        // "move" trough all the possible cursor_ids with a LIMIT step
+        while let Some(cursor) = cursor_id {
+            match reqwest::get(&format!(
+                "http://localhost:17732/v2/log?limit={}&cursor_id={}",
+                LIMIT, cursor
+            ))
+            .await
+            {
+                Ok(result) => {
+                    zip_file.write(&result.bytes().await?)?;
+                    zip_file.write(",".as_bytes())?;
+                }
+                Err(e) => bail!("Error collecting node logs from debugger: {:?}", e),
+            }
+            cursor_id = cursor.checked_sub(LIMIT);
+        }
+
+        zip_file.write("]".as_bytes())?;
+        zip_file.finish()?;
+        Ok(file_path)
     }
 
     pub async fn monitor_stack(&self) -> Result<(), failure::Error> {
