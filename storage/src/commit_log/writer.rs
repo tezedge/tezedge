aab58d83858e2a8b3ba20bc56ec8556bdedc63a3
Mambisi mambisi.zempare@simplestaking.com <lilbizi96@gmail.com>
2021-03-09 13:37:34 +0000

    Read error promogation

--- storage/src/commit_log/writer.rs
+++ storage/src/commit_log/writer.rs
@@ -1,14 +1,13 @@
-use crate::persistent::commit_log::error::TezedgeCommitLogError;
-use crate::persistent::commit_log::{Index, DATA_FILE_NAME, INDEX_FILE_NAME, TH_LENGTH};
+use crate::commit_log::error::TezedgeCommitLogError;
+use crate::commit_log::{Index, DATA_FILE_NAME, INDEX_FILE_NAME, TH_LENGTH};
 use std::fs::{File, OpenOptions};
 use std::io::{BufWriter, Seek, SeekFrom, Write};
 use std::ops::Sub;
 use std::path::{Path, PathBuf};
 
 pub(crate) struct Writer {
-    index_file: BufWriter<File>,
-    data_file: BufWriter<File>,
-    last_index: i64,
+    index_file: File,
+    data_file: File,
 }
 
 impl Writer {
@@ -40,37 +39,33 @@ impl Writer {
             .read(true)
             .open(data_file_path.as_path())?;
 
-        let last_index = Self::last_index(index_file.try_clone()?);
-
         Ok(Self {
-            index_file: BufWriter::new(index_file),
-            data_file: BufWriter::new(data_file),
-            last_index,
+            index_file,
+            data_file,
         })
     }
 
     pub(crate) fn write(&mut self, buf: &[u8]) -> Result<u64, TezedgeCommitLogError> {
-        if buf.len() > u64::MAX as usize {
-            return Err(TezedgeCommitLogError::MessageLengthError);
-        }
-        let mut out = vec![];
-        let uncompressed_length = buf.len();
         {
-            let mut wtr = snap::write::FrameEncoder::new(&mut out);
-            wtr.write_all(buf)?;
+            let mut index_file_buf_writer = BufWriter::new(&mut self.index_file);
+            let mut data_file_buf_writer = BufWriter::new(&mut self.data_file);
+            if buf.len() > u64::MAX as usize {
+                return Err(TezedgeCommitLogError::MessageLengthError);
+            }
+            let message_len = buf.len() as u64;
+            let message_pos = data_file_buf_writer.seek(SeekFrom::End(0))?;
+            data_file_buf_writer.write_all(&buf)?;
+            let th = Index::new(message_pos, message_len);
+            index_file_buf_writer.seek(SeekFrom::End(0))?;
+            index_file_buf_writer.write_all(&th.to_vec())?;
+            data_file_buf_writer.flush()?;
+            index_file_buf_writer.flush()?;
         }
-        let message_len = out.len() as u64;
-        let message_pos = self.data_file.seek(SeekFrom::End(0))?;
-        self.data_file.write_all(&out)?;
-        let th = Index::new(message_pos, message_len, uncompressed_length as u64);
-        self.index_file.seek(SeekFrom::End(0))?;
-        self.index_file.write(&th.to_vec())?;
-        self.last_index += 1;
-        Ok(self.last_index as u64)
+        Ok(self.last_index() as u64)
     }
 
-    pub fn last_index(index_file: File) -> i64 {
-        let metadata = match index_file.metadata() {
+    pub fn last_index(&self) -> i64 {
+        let metadata = match self.index_file.metadata() {
             Ok(m) => m,
             Err(_) => return -1,
         };
