a5150264d3acd7107470ed37bf7c686a9e5f0064
Bruno Deferrari <bruno.deferrari@viablesystems.io>
2021-04-25 13:32:47 -0300

    Rework custom blocks to fix segmentation faults, add patch_context

--- tezos/new_context/src/initializer.rs
+++ tezos/new_context/src/initializer.rs
@@ -4,7 +4,9 @@
 use std::cell::RefCell;
 use std::{path::PathBuf, rc::Rc};
 
-use crate::{TezedgeContext, TezedgeIndex};
+use ocaml_interop::{BoxRoot, OCaml};
+
+use crate::{PatchContextFunction, TezedgeContext, TezedgeIndex};
 
 #[derive(Debug, Clone)]
 pub enum ContextKvStoreConfiguration {
@@ -14,29 +16,36 @@ pub enum ContextKvStoreConfiguration {
 }
 
 // TODO: are errors not possible here? recheck that
-pub fn initialize_tezedge_index(context_kv_store: &ContextKvStoreConfiguration) -> TezedgeIndex {
-    TezedgeIndex::new(match context_kv_store {
-        ContextKvStoreConfiguration::Sled { path } => {
-            let sled = sled::Config::new()
-                .path(path)
-                .open()
-                .expect("Failed to create/initialize Sled database (db_context)");
-            Rc::new(RefCell::new(
-                crate::kv_store::sled_backend::SledBackend::new(sled),
-            ))
-        }
-        ContextKvStoreConfiguration::InMem => Rc::new(RefCell::new(
-            crate::kv_store::in_memory_backend::InMemoryBackend::new(),
-        )),
-        ContextKvStoreConfiguration::BTreeMap => Rc::new(RefCell::new(
-            crate::kv_store::btree_map::BTreeMapBackend::new(),
-        )),
-    })
+pub fn initialize_tezedge_index(
+    context_kv_store: &ContextKvStoreConfiguration,
+    patch_context: BoxRoot<Option<PatchContextFunction>>,
+) -> TezedgeIndex {
+    TezedgeIndex::new(
+        match context_kv_store {
+            ContextKvStoreConfiguration::Sled { path } => {
+                let sled = sled::Config::new()
+                    .path(path)
+                    .open()
+                    .expect("Failed to create/initialize Sled database (db_context)");
+                Rc::new(RefCell::new(
+                    crate::kv_store::sled_backend::SledBackend::new(sled),
+                ))
+            }
+            ContextKvStoreConfiguration::InMem => Rc::new(RefCell::new(
+                crate::kv_store::in_memory_backend::InMemoryBackend::new(),
+            )),
+            ContextKvStoreConfiguration::BTreeMap => Rc::new(RefCell::new(
+                crate::kv_store::btree_map::BTreeMapBackend::new(),
+            )),
+        },
+        patch_context,
+    )
 }
 
 pub fn initialize_tezedge_context(
     context_kv_store: &ContextKvStoreConfiguration,
 ) -> Result<TezedgeContext, failure::Error> {
-    let index = initialize_tezedge_index(context_kv_store);
+    let patch_context = OCaml::none().root();
+    let index = initialize_tezedge_index(context_kv_store, patch_context);
     Ok(TezedgeContext::new(index, None, None))
 }
