761067e1a639d95b9d6df5b89d0a5a78a5753cd0
Sebastien Chapuis <sebastien@chapu.is>
2021-05-05 12:44:36 +0200

    Add tests to rpc endpoint

--- rpc/src/services/context.rs
+++ rpc/src/services/context.rs
@@ -46,7 +46,10 @@ pub(crate) struct BlockStats {
 
 pub(crate) fn make_context_stats() -> Result<ContextStats, failure::Error> {
     let sql = Connection::open("context_stats.db")?;
+    make_stats(sql)
+}
 
+fn make_stats(sql: Connection) -> Result<ContextStats, failure::Error> {
     let blocks = get_blocks_stats(&sql)?;
     let blocks = get_actions_stats(&sql, blocks)?;
 
@@ -184,7 +187,10 @@ fn get_blocks_stats(
     let mut rows = stmt.query([])?;
 
     while let Some(row) = rows.next()? {
-        let block_hash: String = row.get(0).unwrap();
+        let block_hash: String = match row.get(0) {
+            Ok(hash) => hash,
+            _ => continue
+        };
 
         let mut block = BlockStats::default();
         block.block_hash = block_hash.clone();
@@ -200,10 +206,80 @@ fn get_blocks_stats(
 
 #[cfg(test)]
 mod tests {
+    use rusqlite::Batch;
+
     use super::*;
 
     #[test]
     fn test_read_db() {
-        make_context_stats().unwrap();
+        let sql = Connection::open_in_memory().unwrap();
+
+        let schema = include_str!("../../../tezos/new_context/src/schema_stats.sql");
+        let mut batch = Batch::new(&sql, schema);
+        while let Some(mut stmt) = batch.next().unwrap() {
+            stmt.execute([]).unwrap();
+        }
+
+        sql.execute(
+            "
+        UPDATE
+          global_stats
+        SET
+          actions_count = 2,
+          tezedge_checkouts_max = 1.0,
+          tezedge_checkouts_mean = 1.0,
+          tezedge_checkouts_total = 1.0,
+          irmin_checkouts_max = 1.0,
+          irmin_checkouts_mean = 1.0,
+          irmin_checkouts_total = 1.0,
+          tezedge_commits_max = 2.0,
+          tezedge_commits_mean = 2.0,
+          tezedge_commits_total = 2.0,
+          irmin_commits_max = 1.0,
+          irmin_commits_mean = 1.0,
+          irmin_commits_total = 1.0
+        WHERE
+          id = 0;
+            ",
+            [],
+        ).unwrap();
+
+        sql.execute(
+            "INSERT INTO blocks (id, hash) VALUES (1, '1111');",
+            []
+        ).unwrap();
+
+        sql.execute(
+            "
+        INSERT INTO actions
+          (name, key, irmin_time, tezedge_time, block_id, operation_id, context_id)
+        VALUES
+          ('mem', 'a/b/c', 1.2, 1.3, 1, NULL, NULL),
+          ('add', 'a/b/c/d', 1.5, 1.6, 1, NULL, NULL);
+            ",
+            []
+        ).unwrap();
+
+        sql.execute(
+            "
+         INSERT INTO block_details
+           (block_id, action_name, irmin_time, tezedge_time)
+         VALUES
+           (1, 'mem', 1.2, 1.3),
+           (1, 'add', 1.5, 1.6);
+            ",
+            []
+        ).unwrap();
+
+        let stats = make_stats(sql).unwrap();
+
+        assert_eq!(stats.actions_count, 2);
+        assert_eq!(stats.checkout_context.max, 1.0);
+        assert_eq!(stats.commit_context.max, 2.0);
+        assert_eq!(stats.operations_context.len(), 1);
+        assert_eq!(stats.operations_context[0].block_hash, "1111");
+        assert_eq!(stats.operations_context[0].mem, 1.3);
+        assert_eq!(stats.operations_context[0].add, 1.6);
+        assert_eq!(stats.operations_context[0].fold, 0.0);
     }
 }
