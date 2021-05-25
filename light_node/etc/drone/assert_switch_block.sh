c9c42cdfecac76b77eed385efafa15c664de0b0e
Adrian Nagy <adrian.nagy@simplestaking.com>
2021-05-25 21:33:41 +0200

    CI: switch user
    CI: Fix dependency
    
    CI: test
    
    CI: test fix
    
    CI: missing volume
    
    CI: fix log file and test script exit 1
    
    CI: enable build
    
    CI: log level debug, fix proto switch level
    
    Ci: add data cleanup
    
    CI: switch dbs
    
    CI: test-disable build
    
    CI: test-add-peers
    
    CI: enable build
    
    CI: node-enable bootstrap lookup
    
    CI: node - add missing arguments

--- light_node/etc/drone/assert_switch_block.sh
+++ light_node/etc/drone/assert_switch_block.sh
@@ -5,6 +5,7 @@ THRESHOLD=$2
 LOG_FILE=$3
 SWITCH_BLOCK=$4
 
+COUNT=0
 while true
 do
 	LEVEL=$(curl -s $URL | jq .level)
@@ -14,16 +15,28 @@ do
         break
     fi
 
+    # timeout for node to reach the desired block
+    if [[ $COUNT == 600 ]]; then
+        exit 1
+    fi
+
+    ((++COUNT))
 	sleep 1
 done
 
 echo "Switch block was applied"
 
-# grep "validation_result_message: lvl $SWITCH_BLOCK," $LOG_FILE #| tr ',' '\n'
+# Check if the file exists, the timeout it low, because once the node starts responding to the RPC above, the log file should be already create
+./wait_file $LOG_FILE 1
 
 APPLY_TIME=$(grep "validation_result_message: lvl $SWITCH_BLOCK," $LOG_FILE | tr ',' '\n' | grep "protocol_call_elapsed" | awk '{print $2}')
 
 if [[ $APPLY_TIME -gt $THRESHOLD ]]; then
     echo "Block application too long: $APPLY_TIME ms"
     exit 1
+else
+    echo "Block application within threshold: $APPLY_TIME ms"
+    exit 0
 fi
+
+exit 1
