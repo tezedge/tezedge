3c5bea3fad8fabe6d1b663205115a931db0a10ff
Zura Benashvili <zura.benashvili@gmail.com>
2021-05-27 11:33:57 +0400

    refactor

---
+++ tezedge_state/src/proposals/peer_message/mod.rs
@@ -0,0 +1,19 @@
+use tezos_messages::p2p::binary_message::BinaryChunk;
+use tezos_messages::p2p::encoding::prelude::{ConnectionMessage, MetadataMessage, AckMessage};
+
+use crate::PeerCrypto;
+
+mod peer_binary_message;
+pub use peer_binary_message::*;
+
+pub enum PeerMessageError {
+    InvalidMessage,
+}
+
+pub trait PeerMessage {
+    fn take_binary_chunk(self) -> BinaryChunk;
+    fn binary_chunk(&self) -> &BinaryChunk;
+    fn as_connection_msg(&mut self) -> Result<ConnectionMessage, PeerMessageError>;
+    fn as_metadata_msg(&mut self, crypto: &mut PeerCrypto) -> Result<MetadataMessage, PeerMessageError>;
+    fn as_ack_msg(&mut self, crypto: &mut PeerCrypto) -> Result<AckMessage, PeerMessageError>;
+}
---
+++ tla_sm/src/tick_proposal.rs
@@ -0,0 +1,5 @@
+use std::time::Instant;
+
+pub struct TickProposal {
+    pub at: Instant,
+}
