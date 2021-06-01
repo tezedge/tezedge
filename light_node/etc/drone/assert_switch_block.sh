1e4a9b2d5c34905eae29ae65754ef6143ffd83d6
Adrian Nagy <adrian.nagy@simplestaking.com>
2021-06-01 10:20:32 +0200

    Fix wait_file script path

--- light_node/etc/drone/assert_switch_block.sh
+++ light_node/etc/drone/assert_switch_block.sh
@@ -5,6 +5,8 @@ THRESHOLD=$2
 LOG_FILE=$3
 SWITCH_BLOCK=$4
 
+SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
 COUNT=0
 while true
 do
@@ -27,8 +29,9 @@ done
 echo "Switch block was applied"
 
 # Check if the file exists, the timeout it low, because once the node starts responding to the RPC above, the log file should be already create
-./wait_file $LOG_FILE 1
+$SCRIPTPATH/wait_file $LOG_FILE 1
 
+# We get the application time by parsing the log file produced by the node
 APPLY_TIME=$(grep "validation_result_message: lvl $SWITCH_BLOCK," $LOG_FILE | tr ',' '\n' | grep "protocol_call_elapsed" | awk '{print $2}')
 
 if [[ $APPLY_TIME -gt $THRESHOLD ]]; then
