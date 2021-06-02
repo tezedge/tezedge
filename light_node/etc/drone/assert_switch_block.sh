76389ee0eedaee03887501e77e792daaabf332ed
Adrian Nagy <adrian.nagy@simplestaking.com>
2021-06-02 11:45:04 +0200

    Better logs for test script

--- light_node/etc/drone/assert_switch_block.sh
+++ light_node/etc/drone/assert_switch_block.sh
@@ -9,24 +9,31 @@ SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 COUNT=0
 while true
-do
+do  
+    # timeout for node to reach the desired block
+    if [[ $COUNT == 600 ]]; then
+        echo "Block with protocol switch was not applied in the defined timeout "
+        exit 1
+    fi
+
+    ((++COUNT))
+
 	LEVEL=$(curl -s $URL | jq .level)
-    echo "Head at $LEVEL"
+    
+    if [[ -z "$LEVEL" ]]; then
+        echo "Waiting for node"
+    else
+        echo "Head at $LEVEL"
+    fi
 
     if [[ $LEVEL -gt $SWITCH_BLOCK ]]; then
         break
     fi
 
-    # timeout for node to reach the desired block
-    if [[ $COUNT == 600 ]]; then
-        exit 1
-    fi
-
-    ((++COUNT))
 	sleep 1
 done
 
-echo "Switch block was applied"
+echo "Protocol switch block was applied"
 
 # Check if the file exists, the timeout it low, because once the node starts responding to the RPC above, the log file should be already create
 $SCRIPTPATH/wait_file.sh $LOG_FILE 1
@@ -35,10 +42,10 @@ $SCRIPTPATH/wait_file.sh $LOG_FILE 1
 APPLY_TIME=$(grep "validation_result_message: lvl $SWITCH_BLOCK," $LOG_FILE | tr ',' '\n' | grep "protocol_call_elapsed" | awk '{print $2}')
 
 if [[ $APPLY_TIME -gt $THRESHOLD ]]; then
-    echo "Block application too long: $APPLY_TIME ms"
+    echo "Protocol switch block application too long: $APPLY_TIME ms"
     exit 1
 else
-    echo "Block application within threshold: $APPLY_TIME ms"
+    echo "Protocol switch block application within threshold: $APPLY_TIME ms"
     exit 0
 fi
 
