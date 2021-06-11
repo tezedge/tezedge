99f7f0cb0927f1461ede4a40e46b312b4eadc6d2
Zura Benashvili <zura.benashvili@gmail.com>
2021-06-11 12:07:20 +0400

    test(simulator): simulate disconnects

--- tezedge_state/tests/simulate_one_peer_all_message_sequences.rs
+++ tezedge_state/tests/simulate_one_peer_all_message_sequences.rs
@@ -1,12 +1,9 @@
 use std::time::{Instant, Duration};
-use hex::FromHex;
 use quickcheck::{Arbitrary, Gen};
 use itertools::Itertools;
 
 use crypto::nonce::Nonce;
 use crypto::proof_of_work::ProofOfWork;
-use crypto::hash::{HashTrait, CryptoboxPublicKeyHash};
-use crypto::crypto_box::{CryptoKey, PublicKey, SecretKey};
 use tezos_messages::p2p::binary_message::{BinaryChunk, BinaryMessage};
 use tezos_messages::p2p::encoding::prelude::{ConnectionMessage, MetadataMessage, NetworkVersion};
 use tezos_messages::p2p::encoding::ack::{AckMessage, NackInfo};
@@ -16,26 +13,11 @@ use tezedge_state::proposals::*;
 use tezedge_state::proposals::peer_message::*;
 use tezedge_state::acceptors::*;
 
+pub mod common;
+use common::identities::identity_1;
+
 fn network_version() -> NetworkVersion {
     NetworkVersion::new("EDONET".to_string(), 0, 0)
-}
-
-fn identity(pkh: &[u8], pk: &[u8], sk: &[u8], pow: &[u8]) -> Identity {
-    Identity {
-        peer_id: CryptoboxPublicKeyHash::try_from_bytes(pkh).unwrap(),
-        public_key: PublicKey::from_bytes(pk).unwrap(),
-        secret_key: SecretKey::from_bytes(sk).unwrap(),
-        proof_of_work_stamp: ProofOfWork::from_hex(hex::encode(pow)).unwrap(),
-    }
-}
-
-fn identity_1() -> Identity {
-    identity(
-        &[86, 205, 231, 178, 152, 146, 2, 157, 213, 131, 90, 117, 83, 132, 177, 84],
-        &[148, 73, 141, 148, 22, 20, 15, 188, 69, 132, 149, 51, 61, 170, 193, 180, 200, 126, 65, 159, 87, 38, 113, 122, 84, 249, 182, 198, 116, 118, 174, 28],
-        &[172, 122, 207, 58, 254, 215, 99, 123, 225, 15, 143, 199, 106, 46, 182, 179, 53, 156, 120, 173, 177, 216, 19, 180, 28, 186, 179, 250, 233, 84, 244, 177],
-        &[187, 194, 48, 1, 73, 36, 158, 28, 204, 132, 165, 67, 98, 35, 108, 60, 187, 194, 204, 47, 251, 211, 182, 234],
-    )
 }
 
 #[derive(Debug, Clone)]
