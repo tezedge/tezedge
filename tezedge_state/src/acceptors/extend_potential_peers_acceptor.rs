b5d3e070c937cb3c80bf183050a59e4d1cc6ff6b
Zura Benashvili <zura.benashvili@gmail.com>
2021-06-11 12:09:25 +0400

    test(tezedge_state): panic if acceptor receives outdated proposal during `test`

--- tezedge_state/src/acceptors/extend_potential_peers_acceptor.rs
+++ tezedge_state/src/acceptors/extend_potential_peers_acceptor.rs
@@ -6,7 +6,9 @@ impl<P> Acceptor<ExtendPotentialPeersProposal<P>> for TezedgeState
     where P: IntoIterator<Item = PeerAddress>,
 {
     fn accept(&mut self, proposal: ExtendPotentialPeersProposal<P>) {
-        if let Err(_) = self.validate_proposal(&proposal) {
+        if let Err(_err) = self.validate_proposal(&proposal) {
+            #[cfg(test)]
+            assert_ne!(_err, crate::InvalidProposalError::ProposalOutdated);
             return;
         }
         self.extend_potential_peers(proposal.peers);
