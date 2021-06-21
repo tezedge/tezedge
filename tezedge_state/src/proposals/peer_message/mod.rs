72c95840c90c2e28811e9ba41f9622ef22d79cd2
Zura Benashvili <zura.benashvili@gmail.com>
2021-06-21 06:26:58 +0400

    feat(tezedge_state): implement decryption and decoding for PeerMessageResponse

--- tezedge_state/src/proposals/peer_message/mod.rs
+++ tezedge_state/src/proposals/peer_message/mod.rs
@@ -1,4 +1,7 @@
+use crypto::CryptoError;
+use tezos_encoding::binary_reader::BinaryReaderError;
 use tezos_messages::p2p::binary_message::BinaryChunk;
+use tezos_messages::p2p::encoding::peer::PeerMessageResponse;
 use tezos_messages::p2p::encoding::prelude::{ConnectionMessage, MetadataMessage, AckMessage};
 
 use crate::PeerCrypto;
@@ -9,9 +12,23 @@ pub use peer_binary_message::*;
 mod peer_decoded_message;
 pub use peer_decoded_message::*;
 
-#[derive(Debug, Clone)]
+#[derive(Debug)]
 pub enum PeerMessageError {
     InvalidMessage,
+    Crypto(CryptoError),
+    Decode(BinaryReaderError),
+}
+
+impl From<BinaryReaderError> for PeerMessageError {
+    fn from(err: BinaryReaderError) -> Self {
+        Self::Decode(err)
+    }
+}
+
+impl From<CryptoError> for PeerMessageError {
+    fn from(err: CryptoError) -> Self {
+        Self::Crypto(err)
+    }
 }
 
 pub trait PeerMessage {
@@ -20,4 +37,5 @@ pub trait PeerMessage {
     fn as_connection_msg(&mut self) -> Result<ConnectionMessage, PeerMessageError>;
     fn as_metadata_msg(&mut self, crypto: &mut PeerCrypto) -> Result<MetadataMessage, PeerMessageError>;
     fn as_ack_msg(&mut self, crypto: &mut PeerCrypto) -> Result<AckMessage, PeerMessageError>;
+    fn as_peer_msg(&mut self, crypto: &mut PeerCrypto) -> Result<PeerMessageResponse, PeerMessageError>;
 }
