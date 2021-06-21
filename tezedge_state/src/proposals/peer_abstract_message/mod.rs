6bbc49db47c5df706201a9bedef4943eb6df3cfe
Zura Benashvili <zura.benashvili@gmail.com>
2021-06-21 11:32:50 +0400

    feat(tezedge_state): handle advertise message inside state machine

--- tezedge_state/src/proposals/peer_abstract_message/mod.rs
+++ tezedge_state/src/proposals/peer_abstract_message/mod.rs
@@ -1,7 +1,7 @@
 use crypto::CryptoError;
 use tezos_encoding::binary_reader::BinaryReaderError;
 use tezos_messages::p2p::binary_message::BinaryChunk;
-use tezos_messages::p2p::encoding::peer::PeerMessageResponse;
+use tezos_messages::p2p::encoding::peer::PeerMessage;
 use tezos_messages::p2p::encoding::prelude::{ConnectionMessage, MetadataMessage, AckMessage};
 
 use crate::PeerCrypto;
@@ -13,29 +13,29 @@ mod peer_decoded_message;
 pub use peer_decoded_message::*;
 
 #[derive(Debug)]
-pub enum PeerMessageError {
+pub enum PeerAbstractMessageError {
     InvalidMessage,
     Crypto(CryptoError),
     Decode(BinaryReaderError),
 }
 
-impl From<BinaryReaderError> for PeerMessageError {
+impl From<BinaryReaderError> for PeerAbstractMessageError {
     fn from(err: BinaryReaderError) -> Self {
         Self::Decode(err)
     }
 }
 
-impl From<CryptoError> for PeerMessageError {
+impl From<CryptoError> for PeerAbstractMessageError {
     fn from(err: CryptoError) -> Self {
         Self::Crypto(err)
     }
 }
 
-pub trait PeerMessage {
+pub trait PeerAbstractMessage {
     fn take_binary_chunk(self) -> BinaryChunk;
     fn binary_chunk(&self) -> &BinaryChunk;
-    fn as_connection_msg(&mut self) -> Result<ConnectionMessage, PeerMessageError>;
-    fn as_metadata_msg(&mut self, crypto: &mut PeerCrypto) -> Result<MetadataMessage, PeerMessageError>;
-    fn as_ack_msg(&mut self, crypto: &mut PeerCrypto) -> Result<AckMessage, PeerMessageError>;
-    fn as_peer_msg(&mut self, crypto: &mut PeerCrypto) -> Result<PeerMessageResponse, PeerMessageError>;
+    fn as_connection_msg(&mut self) -> Result<ConnectionMessage, PeerAbstractMessageError>;
+    fn as_metadata_msg(&mut self, crypto: &mut PeerCrypto) -> Result<MetadataMessage, PeerAbstractMessageError>;
+    fn as_ack_msg(&mut self, crypto: &mut PeerCrypto) -> Result<AckMessage, PeerAbstractMessageError>;
+    fn as_peer_msg(&mut self, crypto: &mut PeerCrypto) -> Result<PeerMessage, PeerAbstractMessageError>;
 }
