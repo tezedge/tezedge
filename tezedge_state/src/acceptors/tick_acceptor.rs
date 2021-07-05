89b0e8f173eb58f584a60ee5044d0fec1a1107d9
Zura Benashvili <zura.benashvili@gmail.com>
2021-07-05 09:59:38 +0400

    feat(tezedge_state): from potential peers pick random peers to connect to

--- tezedge_state/src/acceptors/tick_acceptor.rs
+++ tezedge_state/src/acceptors/tick_acceptor.rs
@@ -1,8 +1,8 @@
-use tla_sm::{Proposal, Acceptor};
-use crate::TezedgeState;
+use tla_sm::Acceptor;
+use crate::{TezedgeState, Effects};
 use crate::proposals::TickProposal;
 
-impl<E> Acceptor<TickProposal> for TezedgeState<E> {
+impl<E: Effects> Acceptor<TickProposal> for TezedgeState<E> {
     fn accept(&mut self, proposal: TickProposal) {
         if let Err(_err) = self.validate_proposal(&proposal) {
             #[cfg(test)]
