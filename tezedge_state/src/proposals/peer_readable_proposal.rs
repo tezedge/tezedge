81ea0ddc1510a652e618d41f6c27ff483f6370db
Zura Benashvili <zura.benashvili@gmail.com>
2021-07-06 11:36:42 +0400

    feat(tezedge_state): during handshake only read from peer when we are waiting for a message from them
    So that we don't get flooded by same messages.

--- tezedge_state/src/proposals/peer_readable_proposal.rs
+++ tezedge_state/src/proposals/peer_readable_proposal.rs
@@ -4,6 +4,8 @@ use tla_sm::Proposal;
 
 use crate::PeerAddress;
 
+use super::PeerWritableProposal;
+
 // TODO: there should be difference between 2 kinds of proposals. 1 is
 // normal one, which is meant to be recorded and if replayed will give
 // us exact same state. and 2nd is like this one, which is purely just
@@ -12,13 +14,13 @@ use crate::PeerAddress;
 // and are consumed on read, hence hard to debug. So these types of proposals
 // should simply invoke another proposal like in this case that the chunk
 // is ready so that proposal can be recorded and replayed.
-pub struct PeerReadableProposal<'a, R> {
+pub struct PeerReadableProposal<'a, S> {
     pub at: Instant,
     pub peer: PeerAddress,
-    pub stream: &'a mut R,
+    pub stream: &'a mut S,
 }
 
-impl<'a, R> Debug for PeerReadableProposal<'a, R> {
+impl<'a, S> Debug for PeerReadableProposal<'a, S> {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         f.debug_struct("PeerReadableProposal")
             .field("at", &self.at)
@@ -27,8 +29,18 @@ impl<'a, R> Debug for PeerReadableProposal<'a, R> {
     }
 }
 
-impl<'a, R> Proposal for PeerReadableProposal<'a, R> {
+impl<'a, S> Proposal for PeerReadableProposal<'a, S> {
     fn time(&self) -> Instant {
         self.at
+    }
+}
+
+impl<'a, S> From<PeerWritableProposal<'a, S>> for PeerReadableProposal<'a, S> {
+    fn from(proposal: PeerWritableProposal<'a, S>) -> Self {
+        Self {
+            at: proposal.at,
+            peer: proposal.peer,
+            stream: proposal.stream,
+        }
     }
 }
