b95c2a1fbb12d16659529c35c592237a331353e5
Zura Benashvili <zura.benashvili@gmail.com>
2021-07-07 08:51:44 +0400

    feat(tezedge_state): add distinction between disconnect command and disconnected event proposals

--- tezedge_state/src/acceptors/mod.rs
+++ tezedge_state/src/acceptors/mod.rs
@@ -9,5 +9,6 @@ pub mod peer_readable_acceptor;
 pub mod peer_writable_acceptor;
 pub mod send_peer_message_acceptor;
 pub mod peer_disconnect_acceptor;
+pub mod peer_disconnected_acceptor;
 pub mod peer_blacklist_acceptor;
 pub mod pending_request_acceptor;
