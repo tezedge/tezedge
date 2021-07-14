33c80ce26ad4bed99956177e14f356b8b8c75e83
Zura Benashvili <zura.benashvili@gmail.com>
2021-07-14 12:26:37 +0400

    fix(tezedge_state): infinite loop during handshake

---
+++ simulator/tests/test_is_non_blocking.rs
@@ -0,0 +1,71 @@
+use std::time::{Duration, Instant};
+use std::sync::Arc;
+use std::sync::mpsc::{self, TryRecvError};
+
+use simulator::one_real_node_cluster::*;
+use tezedge_state::{TezedgeConfig, TezedgeState, proposer::TezedgeProposerConfig, sample_tezedge_state};
+
+pub fn black_box<T>(dummy: T) -> T {
+    unsafe {
+        let ret = std::ptr::read_volatile(&dummy);
+        std::mem::forget(dummy);
+        ret
+    }
+}
+
+fn default_state(initial_time: Instant) -> TezedgeState {
+    sample_tezedge_state::build(initial_time, TezedgeConfig {
+        port: 9732,
+        disable_mempool: false,
+        private_node: false,
+        min_connected_peers: 2,
+        max_connected_peers: 3,
+        max_pending_peers: 3,
+        max_potential_peers: 10,
+        periodic_react_interval: Duration::from_millis(250),
+        peer_blacklist_duration: Duration::from_secs(15 * 60),
+        peer_timeout: Duration::from_secs(8),
+        pow_target: 1.0,
+    })
+}
+
+fn default_cluster() -> OneRealNodeCluster {
+    let initial_time = Instant::now();
+    OneRealNodeCluster::new(
+        initial_time,
+        TezedgeProposerConfig {
+            wait_for_events_timeout: Some(Duration::from_millis(250)),
+            events_limit: 1024,
+        },
+        default_state(initial_time),
+    )
+}
+
+#[test]
+fn test_no_infinite_loop_when_write_returns_0() {
+    let control = Arc::new(());
+    let is_running = Arc::downgrade(&control);
+
+    std::thread::spawn(move || {
+        let mut cluster = default_cluster();
+        let peer_id = cluster.init_new_fake_peer();
+
+        cluster
+            .connect_from_node(peer_id)
+            .make_progress()
+            .add_writable_event(peer_id, Some(0))
+            .add_tick_for_current_time();
+
+        while !cluster.is_done() {
+            cluster.make_progress();
+            cluster.assert_state();
+        }
+
+        black_box(control);
+    });
+
+    std::thread::sleep(Duration::from_millis(500));
+    if is_running.upgrade().is_some() {
+        panic!("Thread still running. Maybe infinite loop?");
+    }
+}
