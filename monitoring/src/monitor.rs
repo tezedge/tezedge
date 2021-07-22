257d8cecb104fde5d285a8b75ed3767b0a04ac78
Zura Benashvili <zura.benashvili@gmail.com>
2021-07-22 13:17:38 +0400

    perf: remove extra serialization just for counting message bytes from monitoring
    Include size hint in `PeerMessageResponse` instead.

--- monitoring/src/monitor.rs
+++ monitoring/src/monitor.rs
@@ -94,9 +94,10 @@ impl Monitor {
 
         if let Some(monitor) = self.peer_monitors.get_mut(&msg.peer_address) {
             // TODO: TE-190 - reimplement correctly, now not all messages are counted in (Ack, Metadata, ConnectionMessage is not involved)
-            let size = if let Ok(msg) = msg.message.as_bytes() {
-                msg.len()
+            let size = if let Some(size_hint) = msg.message.size_hint() {
+                *size_hint
             } else {
+                debug!(log, "size_hint not available for received peer message"; "peer" => msg.peer_address.to_string());
                 size_of_val(&msg.message)
             };
             monitor.incoming_bytes(size);
