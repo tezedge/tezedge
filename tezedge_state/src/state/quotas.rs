d5ac210dd48e73901aa9f29355e3c1e1871f40ea
Zura Benashvili <zura.benashvili@gmail.com>
2021-07-28 12:22:32 +0400

    feat(tezedge_state): update throttling quotas code to latest version

--- tezedge_state/src/state/quotas.rs
+++ tezedge_state/src/state/quotas.rs
@@ -1,28 +1,5 @@
 use slog::Logger;
-use tezos_messages::p2p::encoding::peer::PeerMessage;
-
-const THROTTLING_QUOTA_NUM: usize = 18;
-
-const THROTTLING_QUOTA_STRS: [&str; THROTTLING_QUOTA_NUM] = [
-    "Disconnect",
-    "Advertise",
-    "SwapRequest",
-    "SwapAck",
-    "Bootstrap",
-    "GetCurrentBranch",
-    "CurrentBranch",
-    "Deactivate",
-    "GetCurrentHead",
-    "CurrentHead",
-    "GetBlockHeaders",
-    "BlockHeader",
-    "GetOperations",
-    "Operation",
-    "GetProtocols",
-    "Protocol",
-    "GetOperationsForBlocks",
-    "OperationsForBlocks",
-];
+use tezos_messages::p2p::encoding::peer::{PeerMessage, MESSAGE_TYPE_COUNT, MESSAGE_TYPE_TEXTS};
 
 const THROTTLING_QUOTA_RESET_MS_DEFAULT: u64 = 5000; // 5 secs
 
@@ -35,7 +12,7 @@ lazy_static::lazy_static! {
     };
 
     /// Quota reset period, in ms
-    static ref THROTTLING_QUOTA_RESET_MS: u64 = {
+    pub(crate) static ref THROTTLING_QUOTA_RESET_MS: u64 = {
         match std::env::var("THROTTLING_QUOTA_RESET_MS") {
             Ok(v) => v.parse().unwrap_or(THROTTLING_QUOTA_RESET_MS_DEFAULT),
             _ => THROTTLING_QUOTA_RESET_MS_DEFAULT,
@@ -43,7 +20,7 @@ lazy_static::lazy_static! {
     };
 
     /// Quota for tx/rx messages per [THROTTLING_QUOTA_RESET_MS]
-    static ref THROTTLING_QUOTA_MAX: [(isize, isize); THROTTLING_QUOTA_NUM] = {
+    pub(crate) static ref THROTTLING_QUOTA_MAX: [(isize, isize); MESSAGE_TYPE_COUNT] = {
         let mut default = [
             (1, 1), // Disconnect
             (1, 1), // Advertise
@@ -51,7 +28,7 @@ lazy_static::lazy_static! {
             (10, 10), // SwapAck
             (1, 1), // Bootstrap
             (10, 500), // GetCurrentBranch
-            (200, 10), // CurrentBranch
+            (500, 10), // CurrentBranch
             (10, 10), // Deactivate
             (10, 10), // GetCurrentHead
             (500, 500), // CurrentHead
@@ -64,7 +41,7 @@ lazy_static::lazy_static! {
             (5000, 5000), // GetOperationsForBlocks
             (10000, 10000), // OperationsForBlocks
         ];
-        for (i, s) in THROTTLING_QUOTA_STRS.iter().enumerate() {
+        for (i, s) in MESSAGE_TYPE_TEXTS.iter().enumerate() {
             let var = "THROTTLING_QUOTA_".to_owned() + &s.to_uppercase();
             if let Ok(val) = std::env::var(var).or_else(|_| std::env::var("THROTTLING_QUOTA_MAX")) {
                 let q = val.split(",").collect::<Vec<_>>();
@@ -79,9 +56,13 @@ lazy_static::lazy_static! {
     };
 }
 
+fn decrease(q: &mut isize) {
+    *q = q.checked_sub(1).unwrap_or(*q)
+}
+
 #[derive(Debug, Clone)]
 pub struct ThrottleQuota {
-    quotas: [(isize, isize); THROTTLING_QUOTA_NUM],
+    quotas: [(isize, isize); MESSAGE_TYPE_COUNT],
     quota_disabled: bool,
     log: Logger,
 }
@@ -95,99 +76,67 @@ impl ThrottleQuota {
         }
     }
 
-    fn msg_index(msg: &PeerMessage) -> usize {
-        debug_assert!(THROTTLING_QUOTA_NUM == 18);
-        match msg {
-            PeerMessage::Disconnect => 0,
-            PeerMessage::Advertise(_) => 1,
-            PeerMessage::SwapRequest(_) => 2,
-            PeerMessage::SwapAck(_) => 3,
-            PeerMessage::Bootstrap => 4,
-            PeerMessage::GetCurrentBranch(_) => 5,
-            PeerMessage::CurrentBranch(_) => 6,
-            PeerMessage::Deactivate(_) => 7,
-            PeerMessage::GetCurrentHead(_) => 8,
-            PeerMessage::CurrentHead(_) => 9,
-            PeerMessage::GetBlockHeaders(_) => 10,
-            PeerMessage::BlockHeader(_) => 11,
-            PeerMessage::GetOperations(_) => 12,
-            PeerMessage::Operation(_) => 13,
-            PeerMessage::GetProtocols(_) => 14,
-            PeerMessage::Protocol(_) => 15,
-            PeerMessage::GetOperationsForBlocks(_) => 16,
-            PeerMessage::OperationsForBlocks(_) => 17,
-        }
-    }
-
-    fn index_to_str(index: usize) -> &'static str {
-        if index < THROTTLING_QUOTA_NUM {
-            THROTTLING_QUOTA_STRS[index]
-        } else {
-            "<invalid index>"
-        }
-    }
-
-    pub fn can_send(&mut self, msg: &PeerMessage) -> Result<isize, isize> {
-        let index = Self::msg_index(msg);
+    pub fn can_send(&mut self, msg: &PeerMessage) -> bool {
+        let index = msg.index();
         if THROTTLING_QUOTA_MAX[index].0 <= 0 {
-            return Ok(THROTTLING_QUOTA_MAX[index].0);
+            return true;
         }
-        self.quotas[index].0 = self.quotas[index]
-            .0
-            .checked_sub(1)
-            .ok_or(self.quotas[index].0)?;
-        if self.quotas[index].0 >= 0 {
-            self.quotas[index].1 = THROTTLING_QUOTA_MAX[index].1.min(self.quotas[index].1 + 1);
-            Ok(self.quotas[index].0)
-        } else if self.quota_disabled {
-            Ok(self.quotas[index].0)
+        decrease(&mut self.quotas[index].0);
+        if self.quota_disabled || self.quotas[index].0 >= 0 {
+            true
         } else {
-            Err(self.quotas[index].0)
+            if self.quotas[index].0 == -1 {
+                slog::warn!(self.log, "Cannot send message because its send quota is exceeded";
+                      "msg_kind" => msg.get_type_str());
+            }
+            false
         }
     }
 
-    pub fn can_receive(&mut self, msg: &PeerMessage) -> Result<isize, isize> {
-        let index = Self::msg_index(msg);
+    pub fn can_receive(&mut self, msg: &PeerMessage) -> bool {
+        let index = msg.index();
         if THROTTLING_QUOTA_MAX[index].1 <= 0 {
-            return Ok(THROTTLING_QUOTA_MAX[index].1);
+            return true;
         }
-        self.quotas[index].1 = self.quotas[index]
-            .0
-            .checked_sub(1)
-            .ok_or(self.quotas[index].1)?;
-        if self.quotas[index].1 >= 0 {
-            self.quotas[index].0 = THROTTLING_QUOTA_MAX[index].0.min(self.quotas[index].0 + 1);
-            Ok(self.quotas[index].1)
-        } else if self.quota_disabled {
-            Ok(self.quotas[index].1)
+        decrease(&mut self.quotas[index].1);
+        if self.quota_disabled || self.quotas[index].1 >= 0 {
+            true
         } else {
-            Err(self.quotas[index].1)
+            if self.quotas[index].1 == -1 {
+                slog::warn!(self.log, "Cannot receive message because its receive quota is exceeded";
+                      "msg_kind" => msg.get_type_str());
+            }
+            false
         }
+    }
+
+    pub fn iter<'a>(&'a self) -> impl 'a + Iterator<Item = (usize, (isize, isize))> {
+        self.quotas.iter().cloned().enumerate()
     }
 
     pub fn reset_all(&mut self) {
-        for index in 0..THROTTLING_QUOTA_NUM {
+        for index in 0..MESSAGE_TYPE_COUNT {
             let (tx_max, rx_max) = THROTTLING_QUOTA_MAX[index];
-            if self.quotas[index].0 < 0 {
+            let (tx, rx) = self.quotas[index];
+            if tx < 0 {
                 slog::warn!(
                     self.log,
                     "Tx quota is exceeded";
-                    "msg_kind" => Self::index_to_str(index),
+                    "msg_kind" => PeerMessage::type_str_for_message_index(index),
                     "quota" => tx_max,
-                    "amount" => -self.quotas[index].0 + tx_max
+                    "amount" => tx_max.checked_sub(tx).map(|i| i.to_string()).unwrap_or(format!("> {}", isize::max_value()))
                 );
             }
-            if self.quotas[index].1 < 0 {
+            if rx < 0 {
                 slog::warn!(
                     self.log,
                     "Rx quota is exceeded";
-                    "msg_kind" => Self::index_to_str(index),
+                    "msg_kind" => PeerMessage::type_str_for_message_index(index),
                     "quota" => rx_max,
-                    "amount" => -self.quotas[index].1 + rx_max
+                    "amount" => rx_max.checked_sub(rx).map(|i| i.to_string()).unwrap_or(format!("> {}", isize::max_value()))
                 );
             }
-            self.quotas[index].0 = tx_max;
-            self.quotas[index].1 = rx_max;
+            self.quotas[index] = (tx_max, rx_max);
         }
     }
 }
