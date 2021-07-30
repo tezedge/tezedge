efbba03ad742a4173479cc7d26b62e3ba1487a72
Mambisi mambisi.zempare@simplestaking.com <lilbizi96@gmail.com>
2021-07-30 08:17:20 +0000

    revert

--- storage_metrics/src/main.rs
+++ storage_metrics/src/main.rs
@@ -42,8 +42,7 @@ use std::str::FromStr;
 use std::option::Option::Some;
 
 const CHAIN_NAME: &'static str = "TEZOS_MAINNET";
-//set tezos
-const LOCAL_PEER: &'static str = "127.0.0.1:8732";
+const LOCAL_PEER: &'static str = "127.0.0.1:9732";
 
 fn shell_compatibility_version() -> ShellCompatibilityVersion {
     ShellCompatibilityVersion::new(CHAIN_NAME.to_owned(), vec![0], vec![0, 1])
