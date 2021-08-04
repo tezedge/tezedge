2b060e9990fa869cdb47da59e2a0636311408d48
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2021-08-04 08:16:42 +0300

    Fix wait_file script

--- light_node/etc/drone/wait_file.sh
+++ light_node/etc/drone/wait_file.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#!/bin/sh
 file="$1"
 timeout="${2:-15}" # 15 seconds as default timeout
 duration_in_sec=0
@@ -18,5 +18,5 @@ wait_file "$file" $timeout || {
   exit 1
 }
 
-duration_in_sec="$(($timeout - $duration_in_sec))"
+duration_in_sec="$((timeout - duration_in_sec))"
 echo "OK - File '$file' found in $duration_in_sec seconds!"
