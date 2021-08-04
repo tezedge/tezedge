ecf5f35e7c83af3a1621f2c0fb5a88529c28f6a0
Alexander Koptelov <alexander.koptelov@viablesystems.io>
2021-08-04 21:44:06 +0300

    Change permissions of build files before synching (#734)

--- synchronize_ci.sh
+++ synchronize_ci.sh
@@ -6,6 +6,9 @@ RSA_PATH=$3
                                                                             # rt-node
 CI_HOSTS=(65.21.119.66 65.21.119.67 65.21.119.68 65.21.119.69 65.21.119.70 135.181.216.254)
 
+# TODO find better way to make paths writable for all pipelines
+chown 1000:1000 -R $PATH_TO_SYNC/
+
 # exclude pushing host (do not synch with self) 
 for HOST in "${CI_HOSTS[@]}"
 do
