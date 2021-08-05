91298d381d6fe0120646e378f5603da499cea265
Branislav Kontur <branislav.kontur@viablesystems.io>
2021-08-05 15:47:02 +0200

    Add main_chain_id to the tezedge_state + moved Bootstrap and GetCurrentBranch to tezedge_state

--- tezedge_state/src/acceptors/pending_request_acceptor.rs
+++ tezedge_state/src/acceptors/pending_request_acceptor.rs
@@ -1,3 +1,6 @@
+// Copyright (c) SimpleStaking, Viable Systems and Tezedge Contributors
+// SPDX-License-Identifier: MIT
+
 use tezos_messages::p2p::encoding::connection::ConnectionMessage;
 use tla_sm::Acceptor;
 
