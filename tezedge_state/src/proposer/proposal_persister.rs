ba33f87a41eba9359b3192016ac9ad2c925b8fa6
Zura Benashvili <zura.benashvili@gmail.com>
2021-08-13 15:51:17 +0400

    feat(tezedge_state): proposal log persister
    Recording of proposals can be enabled with: `RECORD_PROPOSALS=1`

---
+++ tezedge_state/src/proposer/proposal_persister.rs
@@ -0,0 +1,65 @@
+use std::io::BufWriter;
+use std::sync::{mpsc, Mutex, Arc};
+use std::fs::File;
+use std::io::prelude::*;
+
+use crate::proposals::*;
+
+#[derive(Clone)]
+pub struct ProposalPersisterHandle {
+    sender: Option<mpsc::SyncSender<RecordedProposal>>,
+    join_handle: Arc<Mutex<Option<std::thread::JoinHandle<()>>>>,
+}
+
+impl Drop for ProposalPersisterHandle {
+    fn drop(&mut self) {
+        drop(self.sender.take());
+        if let Some(jh) = self.join_handle.lock().unwrap().take() {
+            jh.join().unwrap();
+        }
+    }
+}
+
+impl ProposalPersisterHandle {
+    pub fn persist<P>(&mut self, proposal: P)
+        where P: Into<RecordedProposal>,
+    {
+        // TODO: better error handling.
+        self.sender.as_mut().unwrap().send(proposal.into())
+            .expect("recorded proposal persister disconnected!");
+    }
+}
+
+pub struct ProposalPersister {
+    channel: mpsc::Receiver<RecordedProposal>,
+}
+
+impl ProposalPersister {
+    pub fn start() -> ProposalPersisterHandle {
+        let (tx, rx) = mpsc::sync_channel(1024);
+
+        let state = Self { channel: rx };
+
+        let join_handle = std::thread::spawn(move || run(state));
+
+        ProposalPersisterHandle {
+            sender: Some(tx),
+            join_handle: Arc::new(Mutex::new(Some(join_handle))),
+        }
+    }
+}
+
+fn run(state: ProposalPersister) {
+    let file = File::create("/tmp/tezedge/recorded_proposal.log").expect("couldn't open/create file for persisting proposals");
+    let mut buf_writer = BufWriter::new(file);
+
+    loop {
+        let proposal = match state.channel.recv() {
+            Ok(proposal) => proposal,
+            Err(_) => break,
+        };
+
+        serde_json::to_writer(&mut buf_writer, &proposal).unwrap();
+    }
+    buf_writer.flush().unwrap();
+}
