0204cfc2fd90f12d3acd9f5d531b2eea856ac807
Zura Benashvili <zura.benashvili@gmail.com>
2022-02-12 01:01:52 +0400

    refactor(shell): remove unused p2p_reader thread

--- shell-integration/src/lib.rs
+++ shell-integration/src/lib.rs
@@ -7,14 +7,12 @@ pub use messages::*;
 pub use oneshot::*;
 pub use retry::*;
 pub use streaming_state::*;
-pub use threads::*;
 
 mod connector;
 mod messages;
 mod oneshot;
 mod retry;
 mod streaming_state;
-mod threads;
 
 #[derive(Debug)]
 pub struct UnsupportedMessageError;
