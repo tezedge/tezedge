1c5bb0f1305f2a5598d9123d44a1ea22b1b8a676
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2022-05-18 10:47:40 +0300

    improve cmd, fix persistance

--- apps/baker/src/bin/baker.rs
+++ apps/baker/src/bin/baker.rs
@@ -3,6 +3,7 @@
 
 use std::{
     fs::{self, File},
+    path::PathBuf,
     sync::{
         atomic::{AtomicBool, Ordering},
         Arc,
@@ -10,15 +11,37 @@ use std::{
     time::SystemTime,
 };
 
+use reqwest::Url;
+use structopt::StructOpt;
+
+#[derive(StructOpt, Debug)]
+pub struct Arguments {
+    #[structopt(long)]
+    base_dir: PathBuf,
+    #[structopt(long)]
+    endpoint: Url,
+    #[structopt(short)]
+    archive: bool,
+    #[allow(dead_code)]
+    #[structopt(short)]
+    log_rpc: bool,
+    #[structopt(long)]
+    node_dir: PathBuf,
+    #[structopt(long)]
+    baker: String,
+}
+
 fn main() {
-    use baker::{machine::*, Arguments, Command, Services};
+    use baker::{machine::*, Services};
     use redux_rs::Store;
 
     let Arguments {
         base_dir,
         endpoint,
         archive,
-        command,
+        log_rpc: _,
+        node_dir,
+        baker,
     } = Arguments::from_args();
 
     let env = env_logger::Env::default().default_filter_or("info");
@@ -27,79 +50,73 @@ fn main() {
         .try_init()
         .unwrap();
 
-    match command {
-        Command::RunWithLocalNode { node_dir, baker } => {
-            // We don't use context storage and protocol_runner
-            let _ = node_dir;
+    // We don't use context storage and protocol_runner
+    let _ = node_dir;
 
-            let (srv, events) = Services::new(endpoint, &base_dir, &baker);
-            let chain_id = srv.client.get_chain_id().unwrap();
-            let _ = srv.client.wait_bootstrapped().unwrap();
-            let constants = srv.client.get_constants().unwrap();
-            srv.client.monitor_heads(&chain_id).unwrap();
-            let log = srv.log.clone();
+    let (srv, events) = Services::new(endpoint, &base_dir, &baker);
+    let chain_id = srv.client.get_chain_id().unwrap();
+    let _ = srv.client.wait_bootstrapped().unwrap();
+    let constants = srv.client.get_constants().unwrap();
+    srv.client.monitor_heads(&chain_id).unwrap();
+    let log = srv.log.clone();
 
-            let terminating = Arc::new(AtomicBool::new(false));
-            signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&terminating))
-                .expect("cannot handle signals");
+    let terminating = Arc::new(AtomicBool::new(false));
+    signal_hook::flag::register(signal_hook::consts::SIGTERM, Arc::clone(&terminating))
+        .expect("cannot handle signals");
 
-            // store the state here, and then atomically swap to avoid corruption
-            // due to unexpected power outage
-            let file_path_swap = base_dir.join(format!(".state_{baker}_{chain_id}.json"));
+    // store the state here, and then atomically swap to avoid corruption
+    // due to unexpected power outage
+    let file_path_swap = base_dir.join(format!(".state_{baker}_{chain_id}.json"));
 
-            let archive_path = base_dir.join(format!("state_{baker}_{chain_id}_archive"));
+    let archive_path = base_dir.join(format!("state_{baker}_{chain_id}_archive"));
+    if archive {
+        let _ = fs::create_dir_all(&archive_path);
+    }
+
+    let file_path = base_dir.join(format!("state_{baker}_{chain_id}.json"));
+    let persistent_state = File::open(&file_path)
+        .and_then(|rdr| serde_json::from_reader::<_, BakerState>(rdr).map_err(From::from));
+
+    let initial_state = if let Ok(persistent_state) = persistent_state {
+        persistent_state
+    } else {
+        BakerState::new(chain_id, constants, srv.crypto.public_key_hash().clone())
+    };
+
+    let initial_state = BakerStateEjectable(Some(initial_state));
+    let reducer = baker_reducer::<BakerStateEjectable, Action>;
+    let effects = baker_effects::<BakerStateEjectable, Services, Action>;
+    let initial_time = SystemTime::now();
+    let mut store = Store::new(reducer, effects, srv, initial_time, initial_state);
+    let mut previous_checkpoint = (0, 0);
+    for event in events {
+        store.dispatch::<BakerAction>(event.action.into());
+        let state = store.state.get().as_ref().as_ref().unwrap();
+        let st = state.as_ref();
+        let this_checkpoint = (
+            st.tb_state.level().unwrap_or(0),
+            st.tb_state.round().unwrap_or(0),
+        );
+        if this_checkpoint != previous_checkpoint {
+            let file_swap = File::create(&file_path_swap).expect("msg");
+            serde_json::to_writer(file_swap, state).unwrap();
+            fs::rename(&file_path_swap, &file_path).unwrap();
+            let _ = fs::remove_file(&file_path_swap);
+            slog::info!(log, "stored on disk");
+
             if archive {
-                let _ = fs::create_dir_all(&archive_path);
+                let name = format!("_{}_{}.json", this_checkpoint.0, this_checkpoint.1);
+                let dst = archive_path.join(name);
+                slog::info!(log, "archive {}", dst.display());
+                fs::copy(&file_path, dst).unwrap();
             }
 
-            let file_path = base_dir.join(format!("state_{baker}_{chain_id}.json"));
-            let persistent_state = File::open(&file_path)
-                .and_then(|rdr| serde_json::from_reader::<_, BakerState>(rdr).map_err(From::from));
-
-            let initial_state = if let Ok(persistent_state) = persistent_state {
-                persistent_state
-            } else {
-                BakerState::new(chain_id, constants, srv.crypto.public_key_hash().clone())
-            };
-
-            let initial_state = BakerStateEjectable(Some(initial_state));
-            let reducer = baker_reducer::<BakerStateEjectable, Action>;
-            let effects = baker_effects::<BakerStateEjectable, Services, Action>;
-            let initial_time = SystemTime::now();
-            let mut store = Store::new(reducer, effects, srv, initial_time, initial_state);
-            let mut previous_is_idle = true;
-            for event in events {
-                store.dispatch::<BakerAction>(event.action.into());
-                let state = store.state.get().as_ref().as_ref().unwrap();
-                if let BakerState::Idle(st) = state {
-                    if !previous_is_idle {
-                        // the state is idle, store the state into file
-                        let file_swap = File::create(&file_path_swap).expect("msg");
-                        serde_json::to_writer(file_swap, state).unwrap();
-                        fs::rename(&file_path_swap, &file_path).unwrap();
-                        let _ = fs::remove_file(&file_path_swap);
-                        slog::info!(log, "stored on disk");
-
-                        if archive {
-                            let name = format!(
-                                "{}_{}",
-                                st.tb_state.level().unwrap_or(0),
-                                st.tb_state.round().unwrap_or(0),
-                            );
-                            fs::copy(&file_path, archive_path.join(name)).unwrap();
-                        }
-
-                        if terminating.load(Ordering::SeqCst) {
-                            // ctrl+c pressed, state is on disk, terminate the baker
-                            slog::info!(log, "terminated gracefully");
-                            break;
-                        }
-                    }
-                    previous_is_idle = true;
-                } else {
-                    previous_is_idle = false;
-                }
+            if terminating.load(Ordering::SeqCst) {
+                // ctrl+c pressed, state is on disk, terminate the baker
+                slog::info!(log, "terminated gracefully");
+                break;
             }
         }
+        previous_checkpoint = this_checkpoint;
     }
 }
