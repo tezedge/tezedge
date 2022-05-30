8e36d73a1f1fca19b88695d6cd763ce0284ad52c
Vladislav Melnik <vladislav.melnik@viablesystems.io>
2022-05-30 11:36:01 +0300

    add tests for timer
    Signed-off-by: Vladislav Melnik <vladislav.melnik@viablesystems.io>

--- apps/baker/src/services/timer.rs
+++ apps/baker/src/services/timer.rs
@@ -78,3 +78,75 @@ impl Drop for Timer {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::{
+        sync::mpsc,
+        thread,
+        time::{Duration, SystemTime},
+    };
+    use tenderbake as tb;
+
+    use crate::machine::BakerAction;
+
+    use super::Timer;
+
+    fn new_timer_and_collector() -> (Timer, tb::Timestamp, thread::JoinHandle<Vec<BakerAction>>) {
+        let (tx, rx) = mpsc::channel();
+        let timer = Timer::spawn(tx);
+        let now = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("the unix epoch has begun");
+        let timestamp = tb::Timestamp { unix_epoch: now };
+
+        let collector = thread::spawn(move || {
+            let mut actions = vec![];
+            while let Ok(action) = rx.recv() {
+                actions.push(action);
+            }
+            actions
+        });
+
+        (timer, timestamp, collector)
+    }
+
+    #[test]
+    fn timer_basic() {
+        let (timer, timestamp, collector) = new_timer_and_collector();
+
+        timer.schedule(timestamp + Duration::from_millis(200), 0, 0);
+        thread::sleep(Duration::from_millis(210));
+        drop(timer);
+
+        let actions = collector.join().unwrap();
+        assert_eq!(actions.len(), 1);
+    }
+
+    // next request should eclipse previous
+    #[test]
+    fn timer_eclipse() {
+        let (timer, timestamp, collector) = new_timer_and_collector();
+
+        timer.schedule(timestamp + Duration::from_millis(200), 0, 0);
+        timer.schedule(timestamp + Duration::from_millis(100), 0, 0);
+        thread::sleep(Duration::from_millis(210));
+        drop(timer);
+
+        let actions = collector.join().unwrap();
+        assert_eq!(actions.len(), 1);
+    }
+
+    // should emit action even if it is late
+    #[test]
+    fn timer_late() {
+        let (timer, timestamp, collector) = new_timer_and_collector();
+
+        thread::sleep(Duration::from_millis(100));
+        timer.schedule(timestamp, 0, 0);
+        drop(timer);
+
+        let actions = collector.join().unwrap();
+        assert_eq!(actions.len(), 1);
+    }
+}
