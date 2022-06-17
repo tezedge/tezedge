033e7d229d2638c62a9a640908586295e4eca7f0
Bruno Deferrari <bruno.deferrari@viablesystems.io>
2022-06-17 12:14:26 -0300

    Debug

--- shell_automaton/src/shutdown/shutdown_reducer.rs
+++ shell_automaton/src/shutdown/shutdown_reducer.rs
@@ -8,19 +8,23 @@ use super::ShutdownState;
 pub fn shutdown_reducer(state: &mut State, action: &ActionWithMeta) {
     match &action.action {
         Action::ShutdownInit(_) => {
+            println!("***** shutdown init");
             state.shutdown = ShutdownState::Init {
                 time: action.time_as_nanos(),
             };
         }
         Action::ShutdownPending(_) => {
+            println!("***** shutdown pending");
             state.shutdown = ShutdownState::pending(action.time_as_nanos());
         }
         Action::ShutdownSuccess(_) => {
+            println!("***** shutdown success");
             state.shutdown = ShutdownState::Success {
                 time: action.time_as_nanos(),
             };
         }
         Action::ProtocolRunnerShutdownSuccess(_) => {
+            println!("***** protocol shutdown success");
             if let ShutdownState::Pending(state) = &mut state.shutdown {
                 state.protocol_runner_shutdown = true
             }
